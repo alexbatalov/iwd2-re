9c3796297fe54e6bd698f378893ab2defa3e2d88
Alexander Batalov <alex.batalov@gmail.com>
2022-11-19 13:04:16 +0300

    Add WinMain

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <afxwin.h>
+
+#include <iostream>
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    std::cout << std::endl;
+    std::cout << "BEGIN LOGGING SESSION";
+    std::cout << std::endl;
+
+    // TODO: Incomplete.
+
+    return 0;
+}
