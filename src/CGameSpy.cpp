7ebf36889aaa5a65a59fc5c0d19d5512c2364452
Alexander Batalov <alex.batalov@gmail.com>
2022-11-21 14:27:22 +0300

    Add CGameSpy constructor and destructor

--- src/CGameSpy.cpp
+++ src/CGameSpy.cpp
@@ -1,13 +1,28 @@
 #include "CGameSpy.h"
 
+// #binary-identical
 // 0x4D0930
 CGameSpy::CGameSpy()
 {
-    // TODO: Incomplete.
+    field_2 = 0;
+    field_12 = 0;
+    field_16 = 0;
+    field_22 = 0;
+    field_1 = 0;
+    field_1E = 0;
+    field_1A = 0;
+    field_28 = 0;
+    field_0 = 0;
+    field_26 = 0;
 }
 
+// #binary-identical
 // 0x4D0960
 CGameSpy::~CGameSpy()
 {
-    // TODO: Incomplete.
+    if (field_0 == 1) {
+        WSACleanup();
+    }
+    field_0 = 0;
+    field_1 = 0;
 }
--- src/CGameSpy.h
+++ src/CGameSpy.h
@@ -1,11 +1,24 @@
 #ifndef CGAMESPY_H_
 #define CGAMESPY_H_
 
+#include <windows.h>
+
 // #guess
 class CGameSpy {
 public:
     CGameSpy();
     ~CGameSpy();
+
+    /* 0000 */ unsigned char field_0;
+    /* 0001 */ unsigned char field_1;
+    /* 0002 */ int field_2;
+    /* 0012 */ int field_12;
+    /* 0016 */ int field_16;
+    /* 001A */ int field_1A;
+    /* 001E */ int field_1E;
+    /* 0022 */ int field_22;
+    /* 0026 */ unsigned char field_26;
+    /* 0028 */ int field_28;
 };
 
 #endif /* CGAMESPY_H_ */
