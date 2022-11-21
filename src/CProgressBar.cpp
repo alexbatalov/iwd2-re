b1fd8f32ecc3c9f493cf5407aa304d5c16302ce3
Alexander Batalov <alex.batalov@gmail.com>
2022-11-21 14:46:14 +0300

    Add CProgressBar::Initialize

--- src/CProgressBar.cpp
+++ src/CProgressBar.cpp
@@ -1,7 +1,25 @@
 #include "CProgressBar.h"
 
+// #binary-identical
 // 0x78BB60
 void CProgressBar::Initialize()
 {
-    // TODO: Incomplete.
+    field_4 = 0;
+    field_8 = 0;
+    field_15 = 0;
+    field_16 = 0;
+    field_10 = 0;
+    field_1C = 0;
+    field_14 = 0;
+    field_C = 0;
+    field_1A = 0;
+    field_1B = 0;
+    m_nSecondsToTimeout = 255;
+
+    for (int k = 0; k < 6; k++) {
+        m_nRemoteActionProgress[k] = 0;
+        m_nRemoteActionTarget[k] = 0;
+        m_bRemoteWaiting[k] = 0;
+        m_nRemoteWaitingReason[k] = 0;
+    }
 }
--- src/CProgressBar.h
+++ src/CProgressBar.h
@@ -1,6 +1,8 @@
 #ifndef CPROGRESSBAR_H_
 #define CPROGRESSBAR_H_
 
+#include <windows.h>
+
 class CProgressBar {
 public:
     // NOTE: The `Initialize` function is called in implicit part of `CChitin`
@@ -11,6 +13,22 @@ public:
     }
 
     void Initialize();
+
+    /* 0000 */ int m_nSecondsToTimeout;
+    /* 0004 */ int field_4;
+    /* 0008 */ int field_8;
+    /* 000C */ int field_C;
+    /* 0010 */ int field_10;
+    /* 0014 */ unsigned char field_14;
+    /* 0015 */ unsigned char field_15;
+    /* 0016 */ int field_16;
+    /* 001A */ unsigned char field_1A;
+    /* 001B */ unsigned char field_1B;
+    /* 001C */ unsigned char field_1C;
+    /* 001D */ BOOLEAN m_bRemoteWaiting[6];
+    /* 0024 */ int m_nRemoteActionProgress[6];
+    /* 003C */ int m_nRemoteActionTarget[6];
+    /* 0054 */ int m_nRemoteWaitingReason[6];
 };
 
 #endif /* CPROGRESSBAR_H_ */
