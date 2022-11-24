8236c989c4bd3bc81e2f7dfd14e93e0632323789
Alexander Batalov <alex.batalov@gmail.com>
2022-11-24 12:49:56 +0300

    Add CBaldurMessage constructor and destructor

--- src/CBaldurMessage.cpp
+++ src/CBaldurMessage.cpp
@@ -1,13 +1,71 @@
 #include "CBaldurMessage.h"
 
+// 0x84CF76
+const BYTE CBaldurMessage::DELETEAREA_EMPTY_VOTE = 101;
+
+// // 0x84CFC4
+const DWORD CBaldurMessage::SIGNAL_SECONDSTOMPTIMEOUT = 200;
+
 // 0x4294F0
 CBaldurMessage::CBaldurMessage()
 {
-    // TODO: Incomplete.
+    field_6 = 0;
+    field_EE = 0;
+    field_0 = 0;
+    field_1 = 0;
+    field_2 = 0;
+    field_3 = 0;
+    field_5 = 0;
+    field_4 = 0;
+    field_D3 = 0;
+    field_D4 = 0;
+    field_D5 = 0;
+    field_D6 = 0;
+    field_DA = 0;
+    field_DB = 0;
+    field_DC = 0;
+    field_DD = 0;
+    field_DE = 0;
+    field_DF = 0;
+    field_74 = "";
+    field_73 = 0;
+    field_78 = 0;
+
+    for (int k = 0; k < 6; k++) {
+        field_82[k] = 0;
+        field_7C[k] = DELETEAREA_EMPTY_VOTE;
+    }
+
+    field_E0 = 0;
+    field_E1 = 0;
+    field_E2 = 0;
+    field_E3 = 0;
+    field_E4 = -1;
+    field_29 = 0;
+    field_2A = 0;
+    m_nSignalQueueSize = 24;
+
+    for (BYTE l = 0; l < m_nSignalQueueSize; l++) {
+        field_2B[l] = -1;
+        field_5B[l] = 0;
+        field_43[l] = 0;
+    }
+
+    field_D2 = 0;
+
+    for (int m = 0; m < 6; m++) {
+        field_BA[m] = 0;
+    }
+
+    field_E5 = 0;
+    m_dwSignalSecondsToTimeout = SIGNAL_SECONDSTOMPTIMEOUT;
+    field_7 = 0;
+    field_F4 = 0;
+    field_F5 = 0;
+    field_F6 = 0;
 }
 
 // 0x429720
 CBaldurMessage::~CBaldurMessage()
 {
-    // TODO: Incomplete.
 }
--- src/CBaldurMessage.h
+++ src/CBaldurMessage.h
@@ -1,14 +1,65 @@
 #ifndef CBALDURMESSAGE_H_
 #define CBALDURMESSAGE_H_
 
+#include <afx.h>
+#include <afxmt.h>
+
 #include "CChatBuffer.h"
 
 class CBaldurMessage {
 public:
+    static const BYTE DELETEAREA_EMPTY_VOTE;
+    static const DWORD SIGNAL_SECONDSTOMPTIMEOUT;
+
     CBaldurMessage();
     ~CBaldurMessage();
 
-    CChatBuffer m_cChatBuffer;
+    /* 0000 */ unsigned char field_0;
+    /* 0001 */ unsigned char field_1;
+    /* 0002 */ unsigned char field_2;
+    /* 0003 */ unsigned char field_3;
+    /* 0004 */ unsigned char field_4;
+    /* 0005 */ unsigned char field_5;
+    /* 0006 */ unsigned char field_6;
+    /* 0007 */ unsigned char field_7;
+    /* 0008 */ CChatBuffer m_cChatBuffer;
+    /* 0028 */ BYTE m_nSignalQueueSize;
+    /* 0029 */ unsigned char field_29;
+    /* 002A */ unsigned char field_2A;
+    /* 002B */ unsigned char field_2B[24];
+    /* 0043 */ unsigned char field_43[24];
+    /* 005B */ unsigned char field_5B[24];
+    /* 0073 */ unsigned char field_73;
+    /* 0074 */ CString field_74;
+    /* 0078 */ int field_78;
+    /* 007C */ unsigned char field_7C[6];
+    /* 0082 */ int field_82[6];
+    /* 009A */ CCriticalSection field_9A;
+    /* 00BA */ int field_BA[6];
+    /* 00D2 */ unsigned char field_D2;
+    /* 00D3 */ unsigned char field_D3;
+    /* 00D4 */ unsigned char field_D4;
+    /* 00D5 */ unsigned char field_D5;
+    /* 00D6 */ int field_D6;
+    /* 00DA */ unsigned char field_DA;
+    /* 00DB */ unsigned char field_DB;
+    /* 00DC */ unsigned char field_DC;
+    /* 00DD */ unsigned char field_DD;
+    /* 00DE */ unsigned char field_DE;
+    /* 00DF */ unsigned char field_DF;
+    /* 00E0 */ unsigned char field_E0;
+    /* 00E1 */ unsigned char field_E1;
+    /* 00E2 */ unsigned char field_E2;
+    /* 00E3 */ unsigned char field_E3;
+    /* 00E4 */ unsigned char field_E4;
+    /* 00E5 */ unsigned char field_E5;
+    /* 00E6 */ CString field_E6;
+    /* 00EA */ CString field_EA;
+    /* 00EE */ unsigned char field_EE;
+    /* 00F0 */ DWORD m_dwSignalSecondsToTimeout;
+    /* 00F4 */ unsigned char field_F4;
+    /* 00F5 */ unsigned char field_F5;
+    /* 00F6 */ unsigned char field_F6;
 };
 
 #endif /* CBALDURMESSAGE_H_ */
