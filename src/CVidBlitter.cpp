c864c3ed71b1ced6aaedf9a7985cb792a4ed42fa
Alexander Batalov <alex.batalov@gmail.com>
2022-11-26 16:02:52 +0300

    Add some CVidBlitter functions

--- src/CVidBlitter.cpp
+++ src/CVidBlitter.cpp
@@ -1,7 +1,8 @@
 #include "CVidBlitter.h"
 
-static void sub_7B67E0(int a1, int a2, int a3, int a4, int a5, int a6);
-static void sub_7B6810(int a1, int a2, int a3, int a4, int a5, int a6);
+#include "CChitin.h"
+#include "CUtil.h"
+#include "CWarp.h"
 
 // 0x7B6780
 CVidBlitter::CVidBlitter()
@@ -14,28 +15,157 @@ CVidBlitter::CVidBlitter()
     field_E6 = 0;
     field_DE = 0;
     field_E2 = 0;
-    field_F8 = sub_7B6810;
-    field_E8 = sub_7B67E0;
-    field_FC = sub_7B6810;
-    field_EC = sub_7B67E0;
-    field_F0 = sub_7B67E0;
-    field_F4 = sub_7B67E0;
+    m_pfnBltFast = &CVidBlitter::DxBltFast;
+    m_pfnBltNoFX = &CVidBlitter::DxBlt;
+    m_pfnSrcKeyBltFast = &CVidBlitter::DxBltFast;
+    m_pfnSrcKeyBltNoFX = &CVidBlitter::DxBlt;
+    m_pfnBltFX = &CVidBlitter::DxBlt;
+    m_pfnSrcKeyBltFX = &CVidBlitter::DxBlt;
 }
 
+// #binary-identical
 // 0x7B67E0
-static void sub_7B67E0(int a1, int a2, int a3, int a4, int a5, int a6)
+HRESULT CVidBlitter::DxBlt(IDirectDrawSurface* lpDDSurface, LPRECT lpDestRect, IDirectDrawSurface* lpDDSrcSurface, LPRECT lpSrcRect, DWORD dwFlags, LPDDBLTFX lpDDBltFx)
 {
-    // TODO: Incomplete.
+    return lpDDSurface->Blt(lpDestRect, lpDDSrcSurface, lpSrcRect, dwFlags, lpDDBltFx);
 }
 
+// #binary-identical
 // 0x7B6810
-static void sub_7B6810(int a1, int a2, int a3, int a4, int a5, int a6)
+HRESULT CVidBlitter::DxBltFast(IDirectDrawSurface* lpDDSurface, LPRECT lpDestRect, IDirectDrawSurface* lpDDSrcSurface, LPRECT lpSrcRect, DWORD dwFlags, LPDDBLTFX lpDDBltFx)
 {
-    // TODO: Incomplete.
+    return lpDDSurface->BltFast(lpDestRect->left, lpDestRect->top, lpDDSrcSurface, lpSrcRect, dwFlags);
 }
 
+// #binary-identical
 // 0x7B6960
 void CVidBlitter::Init()
 {
+    if (g_pChitin->cVideo.m_nBpp != 16) {
+        m_pfnBltNoFX = &CVidBlitter::DxBlt;
+        m_pfnSrcKeyBltNoFX = &CVidBlitter::DxBlt;
+        m_pfnBltFX = &CVidBlitter::DxBlt;
+        m_pfnSrcKeyBltFX = &CVidBlitter::DxBlt;
+        m_pfnBltFast = &CVidBlitter::DxBltFast;
+        m_pfnSrcKeyBltFast = &CVidBlitter::DxBltFast;
+    } else {
+        if (m_bSoftMirrorBlt) {
+            m_pfnBltFX = &CVidBlitter::SoftBlt;
+            m_pfnSrcKeyBltFX = &CVidBlitter::SoftSrcKeyBlt;
+        } else {
+            if (m_bSoftSrcKeyBlt) {
+                m_pfnBltFX = &CVidBlitter::DxBlt;
+                m_pfnSrcKeyBltFX = &CVidBlitter::SoftSrcKeyBlt;
+            } else {
+                m_pfnBltFX = &CVidBlitter::DxBlt;
+                m_pfnSrcKeyBltFX = &CVidBlitter::DxBlt;
+            }
+        }
+
+        if (m_bSoftSrcKeyBltFast) {
+            m_pfnSrcKeyBltFast = &CVidBlitter::SoftSrcKeyBlt;
+        } else {
+            m_pfnSrcKeyBltFast = &CVidBlitter::DxBltFast;
+        }
+
+        if (m_bSoftSrcKeyBlt) {
+            m_pfnSrcKeyBltNoFX = &CVidBlitter::SoftSrcKeyBlt;
+        } else {
+            m_pfnSrcKeyBltNoFX = &CVidBlitter::DxBlt;
+        }
+
+        if (m_bSoftBltFast) {
+            m_pfnBltFast = &CVidBlitter::SoftBlt;
+        } else {
+            m_pfnBltFast = &CVidBlitter::DxBltFast;
+        }
+
+        if (m_bSoftBlt) {
+            m_pfnBltNoFX = &CVidBlitter::SoftBlt;
+        } else {
+            m_pfnBltNoFX = &CVidBlitter::DxBlt;
+        }
+    }
+}
+
+// 0x7B6A40
+void CVidBlitter::Reset()
+{
+    if (g_pChitin->pActiveEngine != NULL) {
+        g_pChitin->pActiveEngine->EngineDeactivated();
+    }
+
+    g_pChitin->field_E0 = 1;
+
+    CVidMode* pVidMode = g_pChitin->cVideo.m_pCurrentVidMode;
+    g_pChitin->cVideo.CleanUp();
+    g_pChitin->cVideo.m_pCurrentVidMode = pVidMode;
+
+    if (!g_pChitin->cVideo.Initialize(g_pChitin->cWnd.GetSafeHwnd(), g_pChitin->m_bFullscreen)) {
+        // __FILE__: C:\Projects\Icewind2\src\chitin\ChVidSoftBlt.cpp
+        // __LINE__: 342
+        UTIL_ASSERT_MSG(FALSE, "Software Blitter re-initialization failed.");
+    }
+
+    g_pChitin->Resume();
+
+    if (g_pChitin->pActiveEngine != NULL) {
+        g_pChitin->pActiveEngine->EngineActivated();
+    }
+
+    field_E6 = 0;
+}
+
+// 0x7B6840
+HRESULT CVidBlitter::Blt(IDirectDrawSurface* lpDDSurface, LPRECT lpDestRect, IDirectDrawSurface* lpDDSrcSurface, LPRECT lpSrcRect, DWORD dwFlags, LPDDBLTFX lpDDBltFx)
+{
+    if ((dwFlags & DDBLT_DDFX) != 0) {
+        if ((dwFlags & DDBLT_KEYSRC) != 0) {
+            return (this->*m_pfnSrcKeyBltFX)(lpDDSurface, lpDestRect, lpDDSrcSurface, lpSrcRect, dwFlags, lpDDBltFx);
+        } else {
+            return (this->*m_pfnBltFX)(lpDDSurface, lpDestRect, lpDDSrcSurface, lpSrcRect, dwFlags, lpDDBltFx);
+        }
+    } else {
+        if ((dwFlags & DDBLT_KEYSRC) != 0) {
+            return (this->*m_pfnSrcKeyBltNoFX)(lpDDSurface, lpDestRect, lpDDSrcSurface, lpSrcRect, dwFlags, lpDDBltFx);
+        } else {
+            return (this->*m_pfnBltNoFX)(lpDDSurface, lpDestRect, lpDDSrcSurface, lpSrcRect, dwFlags, lpDDBltFx);
+        }
+    }
+}
+
+// 0x7B68B0
+HRESULT CVidBlitter::BltFast(IDirectDrawSurface* lpDDSurface, DWORD dwX, DWORD dwY, IDirectDrawSurface* lpDDSrcSurface, LPRECT lpSrcRect, DWORD dwTrans)
+{
+    if ((dwTrans & DDBLTFAST_SRCCOLORKEY) != 0) {
+        RECT dest;
+        dest.left = dwX;
+        dest.top = dwY;
+        dest.right = dwX + lpSrcRect->right - lpSrcRect->left;
+        dest.bottom = dwY + lpSrcRect->bottom - lpSrcRect->top;
+        return (this->*m_pfnSrcKeyBltFast)(lpDDSurface, &dest, lpDDSrcSurface, lpSrcRect, dwTrans, NULL);
+    } else {
+        RECT dest;
+        dest.left = dwX;
+        dest.top = dwY;
+        dest.right = dwX + lpSrcRect->right - lpSrcRect->left;
+        dest.bottom = dwY + lpSrcRect->bottom - lpSrcRect->top;
+        return (this->*m_pfnBltFast)(lpDDSurface, &dest, lpDDSrcSurface, lpSrcRect, dwTrans, NULL);
+    }
+}
+
+// 0x7B7280
+HRESULT CVidBlitter::SoftBlt(IDirectDrawSurface* lpDDSurface, LPRECT lpDestRect, IDirectDrawSurface* lpDDSrcSurface, LPRECT lpSrcRect, DWORD dwFlags, LPDDBLTFX lpDDBltFx)
+{
     // TODO: Incomplete.
+
+    return S_OK;
+}
+
+// 0x7B74F0
+HRESULT CVidBlitter::SoftSrcKeyBlt(IDirectDrawSurface* lpDDSurface, LPRECT lpDestRect, IDirectDrawSurface* lpDDSrcSurface, LPRECT lpSrcRect, DWORD dwFlags, LPDDBLTFX lpDDBltFx)
+{
+    // TODO: Incomplete.
+
+    return S_OK;
 }
--- src/CVidBlitter.h
+++ src/CVidBlitter.h
@@ -2,14 +2,21 @@
 #define CVIDBLITTER_H_
 
 #include <afx.h>
+#include <ddraw.h>
 
 class CVidBlitter {
 public:
-    typedef void(Func1)(int, int, int, int, int, int);
+    typedef HRESULT (CVidBlitter::*BltFunc)(IDirectDrawSurface*, LPRECT, IDirectDrawSurface*, LPRECT, DWORD, LPDDBLTFX);
 
     CVidBlitter();
-
+    HRESULT DxBlt(IDirectDrawSurface* lpDDSurface, LPRECT lpDestRect, IDirectDrawSurface* lpDDSrcSurface, LPRECT lpSrcRect, DWORD dwFlags, LPDDBLTFX lpDDBltFx);
+    HRESULT DxBltFast(IDirectDrawSurface* lpDDSurface, LPRECT lpDestRect, IDirectDrawSurface* lpDDSrcSurface, LPRECT lpSrcRect, DWORD dwFlags, LPDDBLTFX lpDDBltFx);
+    HRESULT Blt(IDirectDrawSurface* lpDDSurface, LPRECT lpDestRect, IDirectDrawSurface* lpDDSrcSurface, LPRECT lpSrcRect, DWORD dwFlags, LPDDBLTFX lpDDBltFx);
+    HRESULT BltFast(IDirectDrawSurface* lpDDSurface, DWORD dwX, DWORD dwY, IDirectDrawSurface* lpDDSrcSurface, LPRECT lpSrcRect, DWORD dwTrans);
     void Init();
+    void Reset();
+    HRESULT SoftBlt(IDirectDrawSurface* lpDDSurface, LPRECT lpDestRect, IDirectDrawSurface* lpDDSrcSurface, LPRECT lpSrcRect, DWORD dwFlags, LPDDBLTFX lpDDBltFx);
+    HRESULT SoftSrcKeyBlt(IDirectDrawSurface* lpDDSurface, LPRECT lpDestRect, IDirectDrawSurface* lpDDSrcSurface, LPRECT lpSrcRect, DWORD dwFlags, LPDDBLTFX lpDDBltFx);
 
     /* 0000 */ BOOLEAN m_bSoftSrcKeyBltFast; // #guess
     /* 0001 */ BOOLEAN m_bSoftBltFast; // #guess
@@ -19,12 +26,12 @@ public:
     /* 00DE */ int field_DE;
     /* 00E2 */ int field_E2;
     /* 00E6 */ unsigned char field_E6;
-    /* 00E8 */ Func1* field_E8;
-    /* 00EC */ Func1* field_EC;
-    /* 00F0 */ Func1* field_F0;
-    /* 00F4 */ Func1* field_F4;
-    /* 00F8 */ Func1* field_F8;
-    /* 00FC */ Func1* field_FC;
+    /* 00E8 */ BltFunc m_pfnBltNoFX; // #guess
+    /* 00EC */ BltFunc m_pfnSrcKeyBltNoFX; // #guess
+    /* 00F0 */ BltFunc m_pfnBltFX; // #guess
+    /* 00F4 */ BltFunc m_pfnSrcKeyBltFX; // #guess
+    /* 00F8 */ BltFunc m_pfnBltFast; // #guess
+    /* 00FC */ BltFunc m_pfnSrcKeyBltFast; // #guess
 };
 
 #endif /* CVIDBLITTER_H_ */
