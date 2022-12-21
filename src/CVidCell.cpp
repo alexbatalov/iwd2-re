46b1c18db263d89935fb319e4cbbdd542f01a490
Alexander Batalov <alex.batalov@gmail.com>
2022-12-21 19:22:15 +0300

    Add some CVidCell functions

--- src/CVidCell.cpp
+++ src/CVidCell.cpp
@@ -1,5 +1,7 @@
 #include "CVidCell.h"
 
+#include "CUtil.h"
+
 // 0x8BAC8C
 BOOL CVidCell::TRANSLUCENT_SHADOWS_ON = TRUE;
 
@@ -7,8 +9,8 @@ BOOL CVidCell::TRANSLUCENT_SHADOWS_ON = TRUE;
 // 0x7ACD70
 CVidCell::CVidCell()
 {
-    field_C4 = 0;
-    field_C6 = 0;
+    m_nCurrentFrame = 0;
+    m_nCurrentSequence = 0;
     field_CC = 0;
     field_D0 = 0;
     field_D6 = 0;
@@ -20,4 +22,159 @@ CVidCell::CVidCell()
 CVidCell::~CVidCell()
 {
     // TODO: Incomplete.
+}
+
+// #binary-identical
+// 0x7AD280
+void CVidCell::AddRangeAffect(USHORT nAffectType, USHORT nRangeId, COLORREF rgbAffectColor, BYTE pulsePeriod)
+{
+    // __FILE__: C:\Projects\Icewind2\src\chitin\ChVidImage.cpp
+    // __LINE__: 523
+    UTIL_ASSERT(nRangeId < CVidPalette::NUM_RANGES);
+
+    switch (nAffectType) {
+    case 0:
+        if (m_paletteAffects.pRangeTints[nRangeId] == NULL) {
+            m_paletteAffects.pRangeTints[nRangeId] = new COLORREF;
+        }
+        *m_paletteAffects.pRangeTints[nRangeId] = rgbAffectColor;
+        m_paletteAffects.aRangeTintPeriods[nRangeId] = pulsePeriod;
+        break;
+    case 1:
+    case 3:
+        if (m_paletteAffects.pRangeAdds[nRangeId] == NULL) {
+            m_paletteAffects.pRangeAdds[nRangeId] = new COLORREF;
+        }
+        *m_paletteAffects.pRangeAdds[nRangeId] = rgbAffectColor;
+        m_paletteAffects.aRangeAddPeriods[nRangeId] = pulsePeriod;
+        break;
+    case 2:
+        if (m_paletteAffects.pRangeLights[nRangeId] == NULL) {
+            m_paletteAffects.pRangeLights[nRangeId] = new COLORREF;
+        }
+        *m_paletteAffects.pRangeLights[nRangeId] = rgbAffectColor;
+        m_paletteAffects.aRangeLightPeriods[nRangeId] = pulsePeriod;
+        break;
+    default:
+        // __FILE__: C:\Projects\Icewind2\src\chitin\ChVidImage.cpp
+        // __LINE__: 523
+        UTIL_ASSERT(FALSE);
+    }
+}
+
+// #binary-identical
+// 0x7AD3A0
+void CVidCell::DeleteResPaletteAffect()
+{
+    if (m_paletteAffects.pRangeTints[0] != NULL) {
+        delete m_paletteAffects.pRangeTints[0];
+        m_paletteAffects.pRangeTints[0] = NULL;
+        m_paletteAffects.aRangeTintPeriods[0] = 0;
+    }
+
+    if (m_paletteAffects.pRangeAdds[0] != NULL) {
+        delete m_paletteAffects.pRangeAdds[0];
+        m_paletteAffects.pRangeAdds[0] = NULL;
+        m_paletteAffects.aRangeAddPeriods[0] = 0;
+    }
+
+    if (m_paletteAffects.pRangeLights[0] != NULL) {
+        delete m_paletteAffects.pRangeLights[0];
+        m_paletteAffects.pRangeLights[0] = NULL;
+        m_paletteAffects.aRangeLightPeriods[0] = 0;
+    }
+}
+
+// #binary-identical
+// 0x7AD400
+void CVidCell::DeleteRangeAffects(USHORT nRangeId)
+{
+    // __FILE__: C:\Projects\Icewind2\src\chitin\ChVidImage.cpp
+    // __LINE__: 635
+    UTIL_ASSERT(nRangeId < CVidPalette::NUM_RANGES);
+
+    if (m_paletteAffects.pRangeTints[nRangeId] != NULL) {
+        delete m_paletteAffects.pRangeTints[nRangeId];
+        m_paletteAffects.pRangeTints[nRangeId] = NULL;
+    }
+
+    if (m_paletteAffects.pRangeAdds[nRangeId] != NULL) {
+        delete m_paletteAffects.pRangeAdds[nRangeId];
+        m_paletteAffects.pRangeAdds[nRangeId] = NULL;
+    }
+
+    if (m_paletteAffects.pRangeLights[nRangeId] != NULL) {
+        delete m_paletteAffects.pRangeLights[nRangeId];
+        m_paletteAffects.pRangeLights[nRangeId] = NULL;
+    }
+}
+
+// #binary-identical
+// 0x7ADFE0
+BOOL CVidCell::FrameAdvance()
+{
+    m_nCurrentFrame++;
+    return TRUE;
+}
+
+// #binary-identical
+// 0x7ADFF0
+BOOL CVidCell::FrameSet(SHORT nFrame)
+{
+    m_nCurrentFrame = nFrame;
+    return TRUE;
+}
+
+// #binary-identical
+// 0x7AE290
+BOOL CVidCell::SequenceSet(SHORT nSequence)
+{
+    m_nCurrentSequence = nSequence;
+    return TRUE;
+}
+
+// #binary-identical
+// 0x7AE320
+void CVidCell::AddResPaletteAffect(USHORT nAffectType, COLORREF rgbAffectColor, BYTE pulsePeriod)
+{
+    switch (nAffectType) {
+    case 0:
+        if (m_paletteAffects.pRangeTints[0] == NULL) {
+            m_paletteAffects.pRangeTints[0] = new COLORREF;
+        }
+        *m_paletteAffects.pRangeTints[0] = rgbAffectColor;
+        m_paletteAffects.aRangeTintPeriods[0] = pulsePeriod;
+        break;
+    case 1:
+    case 3:
+        if (m_paletteAffects.pRangeAdds[0] == NULL) {
+            m_paletteAffects.pRangeAdds[0] = new COLORREF;
+        }
+        *m_paletteAffects.pRangeAdds[0] = rgbAffectColor;
+        m_paletteAffects.aRangeAddPeriods[0] = pulsePeriod;
+        break;
+    case 2:
+        if (m_paletteAffects.pRangeLights[0] == NULL) {
+            m_paletteAffects.pRangeLights[0] = new COLORREF;
+        }
+        *m_paletteAffects.pRangeLights[0] = rgbAffectColor;
+        m_paletteAffects.aRangeLightPeriods[0] = pulsePeriod;
+        break;
+    default:
+        // __FILE__: C:\Projects\Icewind2\src\chitin\ChVidImage.cpp
+        // __LINE__: 1704
+        UTIL_ASSERT(FALSE);
+    }
+}
+
+// 0x7AE590
+void CVidCell::SuppressTint(SHORT nRange)
+{
+    m_paletteAffects.suppressTints |= 1 << nRange;
+}
+
+// 0x7AE5B0
+void CVidCell::UnsuppressTint(SHORT nRange)
+{
+    m_paletteAffects.suppressTints &= ~(1 << nRange);
 }
--- src/CVidCell.h
+++ src/CVidCell.h
@@ -11,14 +11,23 @@ public:
 
     CVidCell();
     ~CVidCell();
+    void AddRangeAffect(USHORT nAffectType, USHORT nRangeId, COLORREF rgbAffectColor, BYTE pulsePeriod);
+    void DeleteResPaletteAffect();
+    void DeleteRangeAffects(USHORT nRangeId);
+    BOOL FrameAdvance();
+    BOOL FrameSet(SHORT nFrame);
+    BOOL SequenceSet(SHORT nSequence);
+    void AddResPaletteAffect(USHORT nAffectType, COLORREF rgbAffectColor, BYTE pulsePeriod);
+    void SuppressTint(SHORT nRange);
+    void UnsuppressTint(SHORT nRange);
 
     // NOTE: Force this class to have vftable. Remove once real virtual methods
     // are identified.
     virtual void Dummy() { }
 
     /* 00B4 */ CResHelper<CResCellHeader, 1100> field_B4;
-    /* 00C4 */ short field_C4;
-    /* 00C6 */ short field_C6;
+    /* 00C4 */ SHORT m_nCurrentFrame;
+    /* 00C6 */ SHORT m_nCurrentSequence; // #guess
     /* 00C8 */ int field_C8;
     /* 00CC */ int field_CC;
     /* 00D0 */ int field_D0;
