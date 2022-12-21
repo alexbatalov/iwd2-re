f389b081837e9754c74e818cec04493c7c617926
Alexander Batalov <alex.batalov@gmail.com>
2022-12-21 18:53:59 +0300

    Add some CSound functions

--- src/CSound.cpp
+++ src/CSound.cpp
@@ -1,9 +1,89 @@
 #include "CSound.h"
 
+#include "CChitin.h"
+#include "CSoundMixer.h"
+
+// 0x9060EC
+CSoundMixer* CSound::m_pSoundMixer;
+
+// #binary-identical
+// 0x7A8BB0
+CSound::CSound()
+{
+    m_pSoundMixer = &(g_pChitin->cSoundMixer);
+    field_1C = 0;
+    field_34 = 0;
+    field_18 = 0;
+    field_24 = 0;
+    field_54 = 0;
+    m_nChannel = 0;
+    m_nLooping = 0;
+    field_14 = 0;
+    field_28 = 0;
+    field_2C = 0;
+    field_30 = 0;
+    field_3C = 0;
+    pSoundBuffer = NULL;
+    field_4C = 0;
+    field_50 = 0;
+    field_5C = 0;
+    m_nArea = 0;
+    m_nVolume = 100;
+    m_nPriority = INT_MAX;
+    m_nRange = 768;
+}
+
+// 0x7A8C90
+CSound::CSound(CResRef cResRef, int nRange, int nChannel, int nLooping, int nPriority, BOOL bPositionedSound)
+{
+    SetResRef(cResRef, TRUE, TRUE);
+
+    m_pSoundMixer = &(g_pChitin->cSoundMixer);
+    field_1C = 0;
+    field_34 = 0;
+    m_nVolume = 100;
+    field_18 = 0;
+    field_24 = 0;
+    field_54 = 0;
+    field_28 = 0;
+    field_2C = 0;
+    field_30 = 0;
+    m_nChannel = nChannel;
+    m_nPriority = nPriority;
+    if (nRange > 0) {
+        m_nRange = nRange;
+    } else {
+        m_nRange = 1;
+    }
+    field_14 = 0;
+    field_3C = 0;
+    pSoundBuffer = NULL;
+    field_4C = 0;
+    field_50 = 0;
+    field_5C = 0;
+    m_nArea = 0;
+}
+
+// 0x7A8F60
+BOOL CSound::SetFireForget(BOOL bNewFireForget)
+{
+    // TODO: Incomplete.
+
+    return FALSE;
+}
+
 // 0x7AA110
 void CSound::ResetVolume()
 {
     // TODO: Incomplete.
+}
+
+// 0x7AA4B0
+BOOL CSound::SetChannel(int nNewChannel, DWORD nArea)
+{
+    // TODO: Incomplete.
+
+    return FALSE;
 }
 
 // 0x7A9AB0
@@ -27,6 +107,14 @@ BOOL CSound::IsPlaying()
     } else {
         return FALSE;
     }
+}
+
+// 0x7A9B10
+BOOL CSound::Play(BOOL bReplay)
+{
+    // TODO: Incomplete.
+
+    return FALSE;
 }
 
 // 0x7AA710
--- src/CSound.h
+++ src/CSound.h
@@ -1,29 +1,54 @@
 #ifndef CSOUND_H_
 #define CSOUND_H_
 
-#include <afx.h>
+#include "mfc.h"
 
 // clang-format off
 #include <mmsystem.h>
 #include <dsound.h>
 // clang-format on
 
-class CSound : public CObject {
+#include "CResRef.h"
+#include "CResWave.h"
+
+class CSound : public CObject, public CResHelper<CResWave, 4> {
 public:
+    CSound();
+    CSound(CResRef cResRef, int nRange, int nChannel, int nLooping, int nPriority, BOOL bPositionedSound);
+    BOOL SetFireForget(BOOL bNewFireForget);
     void ResetVolume();
+    BOOL SetChannel(int nNewChannel, DWORD nArea);
 
     BOOL IsPlaying();
+    BOOL Play(BOOL bReplay);
     DWORD GetStatus();
     void SetRange(int nNewRange);
 
     BOOL GetLooping() { return m_nLooping; }
     IDirectSoundBuffer* GetBuffer() { return pSoundBuffer; }
 
+    static CSoundMixer* m_pSoundMixer;
+
+    /* 0014 */ int field_14;
+    /* 0018 */ int field_18;
+    /* 001C */ int field_1C;
     /* 0020 */ int m_nRange; // #guess
+    /* 0024 */ int field_24;
+    /* 0028 */ int field_28;
+    /* 002C */ int field_2C;
+    /* 0030 */ int field_30;
+    /* 0034 */ int field_34;
+    /* 0038 */ int m_nVolume;
+    /* 003C */ int field_3C;
     /* 0040 */ int m_nChannel; // #guess
     /* 0044 */ int m_nPriority; // #guess
     /* 0048 */ int m_nLooping; // #guess
+    /* 004C */ int field_4C;
+    /* 0050 */ int field_50;
+    /* 0054 */ int field_54;
     /* 0058 */ IDirectSoundBuffer* pSoundBuffer; // #guess
+    /* 005C */ int field_5C;
+    /* 0060 */ int m_nArea;
 };
 
 #endif /* CSOUND_H_ */
