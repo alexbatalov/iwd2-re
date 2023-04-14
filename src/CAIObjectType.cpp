e6cee7ca66862e990352c9b128ff329c62bfe15f
Alexander Batalov <alex.batalov@gmail.com>
2023-04-14 13:46:10 +0300

    Add some CAIObjectType functions

--- src/CAIObjectType.cpp
+++ src/CAIObjectType.cpp
@@ -1,1 +1,427 @@
 #include "CAIObjectType.h"
+
+#include "CUtil.h"
+
+// 0x847D40
+const int CAIObjectType::INSTANCE_NOT_SPRITE = 0x80000000;
+
+// 0x847D44
+const BYTE CAIObjectType::SPECIALCASE_MYSELF[5] = {
+    1,
+    0,
+    0,
+    0,
+    0,
+};
+
+// 0x8C7748
+const CAIObjectType CAIObjectType::ANYONE(0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0);
+
+// 0x8C7688
+const CAIObjectType CAIObjectType::ANYTHING(EA_ANYTHING, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0);
+
+// 0x8C7708
+const CAIObjectType CAIObjectType::NOONE(255, 255, 255, 255, 255, 255, 255, 255, -1, 0, 0);
+
+// 0x8C76C8
+const CAIObjectType CAIObjectType::NOT_SPRITE(EA_INANIMATE, 255, 255, 255, 255, 255, 255, 255, INSTANCE_NOT_SPRITE, 0, 0);
+
+// 0x8C7788
+const CAIObjectType CAIObjectType::OBJECT_MYSELF(0, 0, 0, 0, 0, 0, 0, 0, -1, SPECIALCASE_MYSELF, CString(""), 0, 0);
+
+// NOTE: Inlined.
+CAIObjectType::CAIObjectType(const CAIObjectType& type)
+{
+    Set(type);
+}
+
+// 0x40AD00
+CAIObjectType::CAIObjectType(BYTE nEnemyAlly, BYTE nGeneral, BYTE nRace, BYTE nSubRace, BYTE nClass, BYTE nSpecific, BYTE nGender, BYTE nAlignment, int nInstance, const BYTE* SpecialCase, const CString& sName, BYTE nAvClass, int nClassMask)
+{
+    m_nRace = nRace;
+    m_nLocationType = CAIOBJECTTYPE_LOCATION_TYPE_NONE;
+    m_nRadius = 0;
+    m_nEnemyAlly = nEnemyAlly;
+    m_nGeneral = nGeneral;
+    m_nSubRace = nSubRace;
+    if (m_nSubRace == -1) {
+        m_nSubRace = 0;
+    }
+    m_nGender = nGender;
+    m_nSpecific = nSpecific;
+    m_nClass = nClass;
+    m_nAlignment = nAlignment;
+    m_nAvClass = nAvClass;
+    if (m_nAvClass == 0) {
+        m_nAvClass = nClass;
+    }
+    m_nClassMask = nClassMask;
+    m_nInstance = nInstance;
+
+    if (SpecialCase != NULL) {
+        m_SpecialCase[0] = SpecialCase[0];
+        m_SpecialCase[1] = SpecialCase[1];
+        m_SpecialCase[2] = SpecialCase[2];
+        m_SpecialCase[3] = SpecialCase[3];
+        m_SpecialCase[4] = SpecialCase[4];
+    } else {
+        m_SpecialCase[0] = 0;
+        m_SpecialCase[1] = 0;
+        m_SpecialCase[2] = 0;
+        m_SpecialCase[3] = 0;
+        m_SpecialCase[4] = 0;
+    }
+
+    m_sName = sName;
+
+    if (m_nClassMask == 0) {
+        switch (m_nClass) {
+        case C_BARBARIAN:
+            m_nClassMask = CLASSMASK_BARBARIAN;
+            break;
+        case C_BARD:
+            m_nClassMask = CLASSMASK_BARD;
+            break;
+        case C_CLERIC:
+            m_nClassMask = CLASSMASK_CLERIC;
+            break;
+        case C_DRUID:
+            m_nClassMask = CLASSMASK_DRUID;
+            break;
+        case C_FIGHTER:
+            m_nClassMask = CLASSMASK_FIGHTER;
+            break;
+        case C_MONK:
+            m_nClassMask = CLASSMASK_MONK;
+            break;
+        case C_PALADIN:
+            m_nClassMask = CLASSMASK_PALADIN;
+            break;
+        case C_RANGER:
+            m_nClassMask = CLASSMASK_RANGER;
+            break;
+        case C_ROGUE:
+            m_nClassMask = CLASSMASK_ROGUE;
+            break;
+        case C_SORCERER:
+            m_nClassMask = CLASSMASK_SORCERER;
+            break;
+        case C_WIZARD:
+            m_nClassMask = CLASSMASK_WIZARD;
+            break;
+        }
+    }
+}
+
+// 0x40AE80
+CAIObjectType::CAIObjectType(BYTE nEnemyAlly, BYTE nGeneral, BYTE nRace, BYTE nSubRace, BYTE nClass, BYTE nSpecific, BYTE nGender, BYTE nAlignment, int nInstance, BYTE nAvClass, int nClassMask)
+{
+    m_nRace = nRace;
+    m_nLocationType = CAIOBJECTTYPE_LOCATION_TYPE_NONE;
+    m_nEnemyAlly = nEnemyAlly;
+    m_nGeneral = nGeneral;
+    m_nSubRace = nSubRace;
+    if (m_nSubRace == -1) {
+        m_nSubRace = 0;
+    }
+    m_nGender = nGender;
+    m_nSpecific = nSpecific;
+    m_nClass = nClass;
+    m_nAlignment = nAlignment;
+    m_nAvClass = nAvClass;
+    if (m_nAvClass == 0) {
+        m_nAvClass = nClass;
+    }
+    m_nClassMask = nClassMask;
+    m_nInstance = nInstance;
+
+    m_SpecialCase[0] = 0;
+    m_SpecialCase[1] = 0;
+    m_SpecialCase[2] = 0;
+    m_SpecialCase[3] = 0;
+    m_SpecialCase[4] = 0;
+
+    m_sName = "";
+
+    if (m_nClassMask == 0) {
+        switch (m_nClass) {
+        case C_BARBARIAN:
+            m_nClassMask = CLASSMASK_BARBARIAN;
+            break;
+        case C_BARD:
+            m_nClassMask = CLASSMASK_BARD;
+            break;
+        case C_CLERIC:
+            m_nClassMask = CLASSMASK_CLERIC;
+            break;
+        case C_DRUID:
+            m_nClassMask = CLASSMASK_DRUID;
+            break;
+        case C_FIGHTER:
+            m_nClassMask = CLASSMASK_FIGHTER;
+            break;
+        case C_MONK:
+            m_nClassMask = CLASSMASK_MONK;
+            break;
+        case C_PALADIN:
+            m_nClassMask = CLASSMASK_PALADIN;
+            break;
+        case C_RANGER:
+            m_nClassMask = CLASSMASK_RANGER;
+            break;
+        case C_ROGUE:
+            m_nClassMask = CLASSMASK_ROGUE;
+            break;
+        case C_SORCERER:
+            m_nClassMask = CLASSMASK_SORCERER;
+            break;
+        case C_WIZARD:
+            m_nClassMask = CLASSMASK_WIZARD;
+            break;
+        }
+    }
+}
+
+// NOTE: Odd location.
+//
+// 0x6F2D70
+CAIObjectType::~CAIObjectType()
+{
+}
+
+// 0x40B6E0
+void CAIObjectType::Set(const CAIObjectType& type)
+{
+    m_nEnemyAlly = type.m_nEnemyAlly;
+    m_nGeneral = type.m_nGeneral;
+    m_nRace = type.m_nRace;
+    m_nSubRace = type.m_nSubRace;
+    if (m_nSubRace == -1) {
+        m_nSubRace = 0;
+    }
+    m_nClass = type.m_nClass;
+    m_nSpecific = type.m_nSpecific;
+    m_nGender = type.m_nGender;
+    m_nAlignment = type.m_nAlignment;
+    m_nInstance = type.m_nInstance;
+    m_nAvClass = type.m_nAvClass;
+    m_nClassMask = type.m_nClassMask;
+
+    m_SpecialCase[0] = type.m_SpecialCase[0];
+    m_SpecialCase[1] = type.m_SpecialCase[1];
+    m_SpecialCase[2] = type.m_SpecialCase[2];
+    m_SpecialCase[3] = type.m_SpecialCase[3];
+    m_SpecialCase[4] = type.m_SpecialCase[4];
+
+    m_nLocationType = type.m_nLocationType;
+    switch (m_nLocationType) {
+    case CAIOBJECTTYPE_LOCATION_TYPE_RECT:
+        m_rect = type.m_rect;
+        break;
+    case CAIOBJECTTYPE_LOCATION_TYPE_POINT:
+        m_ptCenter = type.m_ptCenter;
+        m_nRadius = type.m_nRadius;
+        break;
+    }
+
+    if (m_nClassMask == 0) {
+        switch (m_nClass) {
+        case C_BARBARIAN:
+            m_nClassMask = CLASSMASK_BARBARIAN;
+            break;
+        case C_BARD:
+            m_nClassMask = CLASSMASK_BARD;
+            break;
+        case C_CLERIC:
+            m_nClassMask = CLASSMASK_CLERIC;
+            break;
+        case C_DRUID:
+            m_nClassMask = CLASSMASK_DRUID;
+            break;
+        case C_FIGHTER:
+            m_nClassMask = CLASSMASK_FIGHTER;
+            break;
+        case C_MONK:
+            m_nClassMask = CLASSMASK_MONK;
+            break;
+        case C_PALADIN:
+            m_nClassMask = CLASSMASK_PALADIN;
+            break;
+        case C_RANGER:
+            m_nClassMask = CLASSMASK_RANGER;
+            break;
+        case C_ROGUE:
+            m_nClassMask = CLASSMASK_ROGUE;
+            break;
+        case C_SORCERER:
+            m_nClassMask = CLASSMASK_SORCERER;
+            break;
+        case C_WIZARD:
+            m_nClassMask = CLASSMASK_WIZARD;
+            break;
+        }
+    }
+
+    m_sName = type.m_sName;
+}
+
+// 0x40CAC0
+CString CAIObjectType::GetName() const
+{
+    return m_sName;
+}
+
+// 0x40CC40
+BOOL CAIObjectType::Equal(const CAIObjectType& type) const
+{
+    return m_nEnemyAlly == type.m_nEnemyAlly
+        && m_nGeneral == type.m_nGeneral
+        && m_nRace == type.m_nRace
+        && m_nSubRace == type.m_nSubRace
+        && m_nClassMask == type.m_nClassMask
+        && m_nSpecific == type.m_nSpecific
+        && m_nAlignment == type.m_nAlignment
+        && m_nGender == type.m_nGender
+        && m_nInstance == type.m_nInstance;
+}
+
+// 0x40D430
+void CAIObjectType::SetSpecialCase(const BYTE* SpecialCase)
+{
+    for (int index = 0; index < 5; index++) {
+        m_SpecialCase[index] = SpecialCase[index];
+    }
+}
+
+// 0x40D700
+BOOL CAIObjectType::IsEnemyOf(const CAIObjectType& type) const
+{
+    if (type.m_nEnemyAlly <= EA_GOODCUTOFF && m_nEnemyAlly >= EA_EVILCUTOFF) {
+        return TRUE;
+    }
+
+    if (m_nEnemyAlly <= EA_GOODCUTOFF && type.m_nEnemyAlly >= EA_EVILCUTOFF) {
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+// 0x40D730
+CAIObjectType CAIObjectType::GetEnemyOf() const
+{
+    CAIObjectType enemy(ANYONE);
+    if (m_nEnemyAlly <= EA_GOODCUTOFF) {
+        enemy.m_nEnemyAlly = EA_EVILCUTOFF;
+    } else if (m_nEnemyAlly >= EA_EVILCUTOFF) {
+        enemy.m_nEnemyAlly = EA_GOODCUTOFF;
+    } else {
+        enemy.m_nEnemyAlly = EA_EVILCUTOFF;
+    }
+    return enemy;
+}
+
+// 0x40D7E0
+BOOL CAIObjectType::IsClassValid(BYTE nClass) const
+{
+    if (nClass == 0) {
+        CString sError;
+        sError.Format("CAIObjectType::IsClassValid() - '%s' has no classes set.", GetName());
+        // __FILE__: C:\Projects\Icewind2\src\Baldur\CAIObjectType.cpp
+        // __LINE__: 2818
+        UTIL_ASSERT_MSG(FALSE, sError);
+    }
+
+    return (m_nClassMask & (1 << (nClass - 1))) != 0;
+}
+
+// 0x40D8A0
+BOOL CAIObjectType::sub_40D8A0(const CPoint& pt) const
+{
+    if (m_ptCenter == pt) {
+        return TRUE;
+    }
+
+    if (m_nRadius > 0) {
+        if ((m_ptCenter.x - pt.x) * (m_ptCenter.x - pt.x) + (m_ptCenter.y - pt.y) * (m_ptCenter.y - pt.y) <= m_nRadius * m_nRadius) {
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+// 0x40D900
+BOOL CAIObjectType::sub_40D900(const CPoint& pt) const
+{
+    return PtInRect(m_rect, pt);
+}
+
+// 0x40D920
+BOOL CAIObjectType::sub_40D920(const CPoint& pt) const
+{
+    // NOTE: Jump table at 0x40BA04.
+    switch (m_nLocationType) {
+    case CAIOBJECTTYPE_LOCATION_TYPE_POINT:
+        return sub_40D8A0(pt);
+    case CAIOBJECTTYPE_LOCATION_TYPE_RECT:
+        return sub_40D900(pt);
+    default:
+        return TRUE;
+    }
+}
+
+// 0x40D940
+void CAIObjectType::SetCenter(const CPoint& pt)
+{
+    m_ptCenter = pt;
+    m_nRadius = 0;
+    m_nLocationType = CAIOBJECTTYPE_LOCATION_TYPE_POINT;
+}
+
+// 0x40D960
+void CAIObjectType::SetRect(const CRect& rect)
+{
+    m_rect = rect;
+    m_nLocationType = CAIOBJECTTYPE_LOCATION_TYPE_RECT;
+}
+
+// 0x40D990
+void CAIObjectType::AddClass(BYTE nClass)
+{
+    switch (nClass) {
+    case C_BARBARIAN:
+        m_nClassMask |= CLASSMASK_BARBARIAN;
+        break;
+    case C_BARD:
+        m_nClassMask |= CLASSMASK_BARD;
+        break;
+    case C_CLERIC:
+        m_nClassMask |= CLASSMASK_CLERIC;
+        break;
+    case C_DRUID:
+        m_nClassMask |= CLASSMASK_DRUID;
+        break;
+    case C_FIGHTER:
+        m_nClassMask |= CLASSMASK_FIGHTER;
+        break;
+    case C_MONK:
+        m_nClassMask |= CLASSMASK_MONK;
+        break;
+    case C_PALADIN:
+        m_nClassMask |= CLASSMASK_PALADIN;
+        break;
+    case C_RANGER:
+        m_nClassMask |= CLASSMASK_RANGER;
+        break;
+    case C_ROGUE:
+        m_nClassMask |= CLASSMASK_ROGUE;
+        break;
+    case C_SORCERER:
+        m_nClassMask |= CLASSMASK_SORCERER;
+        break;
+    case C_WIZARD:
+        m_nClassMask |= CLASSMASK_WIZARD;
+        break;
+    }
+}
--- src/CAIObjectType.h
+++ src/CAIObjectType.h
@@ -3,15 +3,50 @@
 
 #include "mfc.h"
 
+#define CLASSMASK_BARBARIAN 0x00000001
+#define CLASSMASK_BARD 0x00000002
+#define CLASSMASK_CLERIC 0x00000004
+#define CLASSMASK_DRUID 0x00000008
+#define CLASSMASK_FIGHTER 0x00000010
+#define CLASSMASK_MONK 0x00000020
+#define CLASSMASK_PALADIN 0x00000040
+#define CLASSMASK_RANGER 0x00000080
+#define CLASSMASK_ROGUE 0x00000100
+#define CLASSMASK_SORCERER 0x00000200
+#define CLASSMASK_WIZARD 0x00000400
+
+#define CAIOBJECTTYPE_LOCATION_TYPE_NONE 0
+#define CAIOBJECTTYPE_LOCATION_TYPE_POINT 1
+#define CAIOBJECTTYPE_LOCATION_TYPE_RECT 2
+
 class CAIObjectType {
 public:
-    /* 0x847C5B */ static const BYTE CAIObjectType::R_HUMAN = 1;
-    /* 0x847C5B */ static const BYTE CAIObjectType::R_ELF = 2;
-    /* 0x847C5B */ static const BYTE CAIObjectType::R_HALF_ELF = 3;
-    /* 0x847C5B */ static const BYTE CAIObjectType::R_DWARF = 4;
-    /* 0x847C5B */ static const BYTE CAIObjectType::R_HALFLING = 5;
-    /* 0x847C5B */ static const BYTE CAIObjectType::R_GNOME = 6;
-    /* 0x847C5B */ static const BYTE CAIObjectType::R_HALF_ORC = 7;
+    /* 0x847C34 */ static const BYTE EA_ALL = 0;
+    /* 0x847C35 */ static const BYTE EA_PC = 2;
+    /* 0x847C36 */ static const BYTE EA_FAMILIAR = 3;
+    /* 0x847C37 */ static const BYTE EA_ALLY = 4;
+    /* 0x847C38 */ static const BYTE EA_CONTROLLED = 5;
+    /* 0x847C39 */ static const BYTE EA_CHARMED = 6;
+    /* 0x847C3A */ static const BYTE EA_0x847C3A = 7;
+    /* 0x847C3B */ static const BYTE EA_GOODCUTOFF = 30;
+    /* 0x847C3C */ static const BYTE EA_CONTROLCUTOFF = 15;
+    /* 0x847C3D */ static const BYTE EA_NOTGOOD = 31;
+    /* 0x847C3E */ static const BYTE EA_NEUTRAL = 128;
+    /* 0x847C3F */ static const BYTE EA_NOTEVIL = 199;
+    /* 0x847C40 */ static const BYTE EA_ENEMY = 255;
+    /* 0x847C41 */ static const BYTE EA_CHARMED_PC = 254;
+    /* 0x847C42 */ static const BYTE EA_EVILCUTOFF = 200;
+    /* 0x847C43 */ static const BYTE EA_ARENAPC = 253;
+    /* 0x847C44 */ static const BYTE EA_INANIMATE = 127;
+    /* 0x847C45 */ static const BYTE EA_ANYTHING = 126;
+
+    /* 0x847C5B */ static const BYTE R_HUMAN = 1;
+    /* 0x847C5C */ static const BYTE R_ELF = 2;
+    /* 0x847C5D */ static const BYTE R_HALF_ELF = 3;
+    /* 0x847C5E */ static const BYTE R_DWARF = 4;
+    /* 0x847C5F */ static const BYTE R_HALFLING = 5;
+    /* 0x847C60 */ static const BYTE R_GNOME = 6;
+    /* 0x847C61 */ static const BYTE R_HALF_ORC = 7;
 
     /* 0x847CA5 */ static const BYTE C_BARBARIAN = 1;
     /* 0x847CA6 */ static const BYTE C_BARD = 2;
@@ -37,6 +72,50 @@ public:
     /* 0x847CFE */ static const BYTE CHAOTIC_GOOD = 0x31;
     /* 0x847CFF */ static const BYTE CHAOTIC_NEUTRAL = 0x32;
     /* 0x847D00 */ static const BYTE CHAOTIC_EVIL = 0x33;
+
+    static const int INSTANCE_NOT_SPRITE;
+    static const BYTE SPECIALCASE_MYSELF[5];
+    static const CAIObjectType ANYONE;
+    static const CAIObjectType ANYTHING;
+    static const CAIObjectType NOONE;
+    static const CAIObjectType NOT_SPRITE;
+    static const CAIObjectType OBJECT_MYSELF;
+
+    CAIObjectType(const CAIObjectType& type);
+    CAIObjectType(BYTE nEnemyAlly, BYTE nGeneral, BYTE nRace, BYTE nSubRace, BYTE nClass, BYTE nSpecific, BYTE nGender, BYTE nAlignment, int nInstance, const BYTE* SpecialCase, const CString& sName, BYTE nAvClass, int nClassMask);
+    CAIObjectType(BYTE nEnemyAlly, BYTE nGeneral, BYTE nRace, BYTE nSubRace, BYTE nClass, BYTE nSpecific, BYTE nGender, BYTE nAlignment, int nInstance, BYTE nAvClass, int nClassMask);
+    ~CAIObjectType();
+    void Set(const CAIObjectType& type);
+    CString GetName() const;
+    BOOL Equal(const CAIObjectType& type) const;
+    void SetSpecialCase(const BYTE* SpecialCase);
+    BOOL IsEnemyOf(const CAIObjectType& type) const;
+    CAIObjectType GetEnemyOf() const;
+    BOOL IsClassValid(BYTE nClass) const;
+    void AddClass(BYTE nClass);
+    BOOL sub_40D8A0(const CPoint& pt) const;
+    BOOL sub_40D900(const CPoint& pt) const;
+    BOOL sub_40D920(const CPoint& pt) const;
+    void SetCenter(const CPoint& pt);
+    void SetRect(const CRect& rect);
+
+    /* 0000 */ CString m_sName;
+    /* 0004 */ BYTE m_nEnemyAlly;
+    /* 0005 */ BYTE m_nGeneral;
+    /* 0006 */ BYTE m_nRace;
+    /* 0007 */ BYTE m_nSubRace;
+    /* 0008 */ BYTE m_nClass;
+    /* 000A */ int m_nInstance;
+    /* 000E */ BYTE m_SpecialCase[5];
+    /* 0013 */ BYTE m_nSpecific;
+    /* 0014 */ BYTE m_nGender;
+    /* 0015 */ BYTE m_nAlignment;
+    /* 0016 */ BYTE m_nAvClass;
+    /* 0008 */ int m_nClassMask;
+    /* 001C */ int m_nLocationType;
+    /* 0020 */ CPoint m_ptCenter;
+    /* 0028 */ int m_nRadius;
+    /* 002C */ CRect m_rect;
 };
 
 #endif /* CAIOBJECTTYPE_H_ */
