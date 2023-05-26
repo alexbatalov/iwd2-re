7777951363f34ced1ea68c140cae97dfe4993bdf
Alexander Batalov <alex.batalov@gmail.com>
2023-05-26 12:07:43 +0300

    Add CAIGroup

---
+++ src/CAIGroup.cpp
@@ -0,0 +1,57 @@
+#include "CAIGroup.h"
+
+#include "CBaldurChitin.h"
+#include "CGameObjectArray.h"
+#include "CInfGame.h"
+
+// 0x4060C0
+LONG CAIGroup::GetGroupLeader()
+{
+    if (m_memberList.GetCount() > 0) {
+        return reinterpret_cast<LONG>(m_memberList.GetHead());
+    }
+
+    return CGameObjectArray::INVALID_OBJECT_ID;
+}
+
+// 0x4060E0
+LONG* CAIGroup::GetGroupList()
+{
+    LONG* pList = new LONG(m_memberList.GetCount());
+
+    int index = 0;
+    POSITION pos = m_memberList.GetHeadPosition();
+    while (pos != NULL) {
+        pList[index++] = reinterpret_cast<LONG>(m_memberList.GetNext(pos));
+    }
+
+    return pList;
+}
+
+// 0x406170
+POSITION CAIGroup::Add(LONG characterId)
+{
+    SHORT portraitNum = g_pBaldurChitin->GetObjectGame()->GetCharacterPortaitNum(characterId);
+    m_groupChanged = TRUE;
+    if (portraitNum == -1) {
+        return m_memberList.AddTail(reinterpret_cast<LONG*>(characterId));
+    }
+
+    POSITION pos = m_memberList.GetHeadPosition();
+    while (pos != NULL) {
+        LONG memberId = reinterpret_cast<LONG>(m_memberList.GetAt(pos));
+        SHORT memberPortraitNum = g_pBaldurChitin->GetObjectGame()->GetCharacterPortaitNum(memberId);
+        if (memberPortraitNum == -1 || memberPortraitNum > portraitNum) {
+            return m_memberList.InsertBefore(pos, reinterpret_cast<LONG*>(characterId));
+        }
+        m_memberList.GetNext(pos);
+    }
+
+    return m_memberList.AddTail(reinterpret_cast<LONG*>(characterId));
+}
+
+// 0x4093E0
+BOOL CAIGroup::InList(LONG characterId)
+{
+    return m_memberList.Find(reinterpret_cast<LONG*>(characterId)) != NULL;
+}
---
+++ src/CAIGroup.h
@@ -0,0 +1,17 @@
+#ifndef CAIGROUP_H_
+#define CAIGROUP_H_
+
+#include "mfc.h"
+
+class CAIGroup {
+public:
+    LONG GetGroupLeader();
+    LONG* GetGroupList();
+    POSITION Add(LONG characterId);
+    BOOL InList(LONG characterId);
+
+    /* 0004 */ BOOL m_groupChanged;
+    /* 0006 */ CTypedPtrList<CPtrList, LONG*> m_memberList;
+};
+
+#endif /* CAIGROUP_H_ */
