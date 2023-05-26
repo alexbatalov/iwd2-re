0d3e6f65f0fa3b8c4cb577d2c8e987a6212ea9f0
Alexander Batalov <alex.batalov@gmail.com>
2023-05-26 12:15:00 +0300

    Add CAIScriptFile

---
+++ src/CAIScriptFile.cpp
@@ -0,0 +1,16 @@
+#include "CAIScriptFile.h"
+
+// 0x8C8438
+const CString CAIScriptFile::IF("IF");
+
+// 0x8C8424
+const CString CAIScriptFile::THEN("THEN");
+
+// 0x8C8410
+const CString CAIScriptFile::END("END");
+
+// 0x8C8430
+const CString CAIScriptFile::RESPONSE("RESPONSE");
+
+// 0x8C8420
+const CString CAIScriptFile::COMMENT("//");
---
+++ src/CAIScriptFile.h
@@ -0,0 +1,15 @@
+#ifndef CAISCRIPTFILE_H_
+#define CAISCRIPTFILE_H_
+
+#include "mfc.h"
+
+class CAIScriptFile {
+public:
+    static const CString IF;
+    static const CString THEN;
+    static const CString END;
+    static const CString RESPONSE;
+    static const CString COMMENT;
+};
+
+#endif /* CAISCRIPTFILE_H_ */
