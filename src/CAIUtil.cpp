5d21b4f6ca2cd01096044b72c861f518eabcddbd
Alexander Batalov <alex.batalov@gmail.com>
2023-07-03 18:02:35 +0300

    Add CAIUtil::ReadBetween

--- src/CAIUtil.cpp
+++ src/CAIUtil.cpp
@@ -1,5 +1,45 @@
 #include "CAIUtil.h"
 
+// 0x421390
+CString CAIUtil::ReadBetween(CString& line, CString sep)
+{
+    CString sTemp;
+    CString sRemainder;
+    CString sBetween;
+
+    if (line.GetLength() < sep.GetLength()) {
+        return CString("");
+    }
+
+    int start = line.Find(sep);
+    if (start < 0) {
+        return CString("");
+    }
+
+    if (line.GetLength() <= sep.GetLength() + start) {
+        sTemp = "";
+    } else {
+        sTemp = line.Right(line.GetLength() - sep.GetLength() - start);
+    }
+
+    int end = sTemp.Find(sep);
+    if (end < 0) {
+        return sTemp;
+    }
+
+    sBetween = sTemp.Left(end);
+
+    if (sTemp.GetLength() <= sep.GetLength() + end) {
+        sRemainder = "";
+    } else {
+        sRemainder = sTemp.Right(sTemp.GetLength() - sep.GetLength() - end);
+    }
+
+    line = sRemainder;
+
+    return sBetween;
+}
+
 // 0x4215F0
 CString CAIUtil::ReadToChar(CString& line, char c)
 {
