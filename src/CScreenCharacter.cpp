855ef426409e7df09a01da1bd1bec0988d240461
Alexander Batalov <alex.batalov@gmail.com>
2023-07-04 11:03:22 +0300

    Add CScreenCharacter token constants

--- src/CScreenCharacter.cpp
+++ src/CScreenCharacter.cpp
@@ -1,5 +1,20 @@
 #include "CScreenCharacter.h"
 
+// 0x8F3324
+const CString CScreenCharacter::TOKEN_SPELLLEVEL("SPELLLEVEL");
+
+// 0x8F3320
+const CString CScreenCharacter::TOKEN_WEAPONNAME("WEAPONNAME");
+
+// 0x8F333C
+const CString CScreenCharacter::TOKEN_NUMBER("number");
+
+// 0x8F332C
+const CString CScreenCharacter::TOKEN_MINIMUM("MINIMUM");
+
+// 0x8F3334
+const CString CScreenCharacter::TOKEN_MAXIMUM("MAXIMUM");
+
 // 0x5D5220
 CScreenCharacter::CScreenCharacter()
 {
--- src/CScreenCharacter.h
+++ src/CScreenCharacter.h
@@ -5,6 +5,12 @@
 
 class CScreenCharacter : public CBaldurEngine {
 public:
+    static const CString TOKEN_SPELLLEVEL;
+    static const CString TOKEN_WEAPONNAME;
+    static const CString TOKEN_NUMBER;
+    static const CString TOKEN_MINIMUM;
+    static const CString TOKEN_MAXIMUM;
+
     CScreenCharacter();
     ~CScreenCharacter();
 };
