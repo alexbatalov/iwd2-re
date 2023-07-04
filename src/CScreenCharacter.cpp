26562e3c9d313c524b196f8776d9d205e35c9067
Alexander Batalov <alex.batalov@gmail.com>
2023-07-04 11:15:48 +0300

    Add some CScreenCharacter functions

--- src/CScreenCharacter.cpp
+++ src/CScreenCharacter.cpp
@@ -26,3 +26,69 @@ CScreenCharacter::~CScreenCharacter()
 {
     // TODO: Incomplete.
 }
+
+// 0x49FC40
+BOOL CScreenCharacter::CheckSystemKeyCtrl()
+{
+    return TRUE;
+}
+
+// 0x5D62F0
+void CScreenCharacter::SetSystemKeyCtrl(BOOLEAN bValue)
+{
+    m_bCtrlKeyDown = bValue;
+}
+
+// 0x5D6300
+BOOL CScreenCharacter::GetShiftKey()
+{
+    return field_1B8 && m_bShiftKeyDown;
+}
+
+// 0x5D6320
+void CScreenCharacter::SetSystemKeyShift(BOOL bValue)
+{
+    m_bShiftKeyDown = bValue;
+}
+
+// 0x5D6330
+BOOL CScreenCharacter::CheckSystemKeyShift()
+{
+    return field_1B8;
+}
+
+// 0x5D6340
+void CScreenCharacter::SetSystemKeyCapsLock(BOOL bValue)
+{
+    m_bCapsLockKeyOn = bValue;
+}
+
+// 0x5D6350
+BOOL CScreenCharacter::GetCapsLockKey()
+{
+    return field_1B8 && m_bCapsLockKeyOn;
+}
+
+// 0x5D6330
+BOOL CScreenCharacter::CheckSystemKeyCapsLock()
+{
+    return field_1B8;
+}
+
+// 0x699600
+SHORT CScreenCharacter::GetNumVirtualKeys()
+{
+    return CSCREENCHARACTER_VIRTUAL_KEYS;
+}
+
+// 0x5D6370
+CKeyInfo* CScreenCharacter::GetVirtualKeys()
+{
+    return m_pVirtualKeys;
+}
+
+// 0x5D6380
+BYTE* CScreenCharacter::GetVirtualKeysFlags()
+{
+    return m_pVirtualKeysFlags;
+}
--- src/CScreenCharacter.h
+++ src/CScreenCharacter.h
@@ -2,6 +2,9 @@
 #define CSCREENCHARACTER_H_
 
 #include "CBaldurEngine.h"
+#include "CKeyInfo.h"
+
+#define CSCREENCHARACTER_VIRTUAL_KEYS 90
 
 class CScreenCharacter : public CBaldurEngine {
 public:
@@ -13,6 +16,24 @@ public:
 
     CScreenCharacter();
     ~CScreenCharacter();
+    /* 0030 */ BOOL CheckSystemKeyCtrl() override;
+    /* 0034 */ void SetSystemKeyCtrl(BOOLEAN bValue) override;
+    /* 003C */ BOOL CheckSystemKeyShift() override;
+    /* 0040 */ void SetSystemKeyShift(BOOL bValue) override;
+    /* 0044 */ BOOL GetShiftKey() override;
+    /* 0050 */ BOOL CheckSystemKeyCapsLock() override;
+    /* 0054 */ void SetSystemKeyCapsLock(BOOL bValue) override;
+    /* 0058 */ BOOL GetCapsLockKey() override;
+    /* 00A0 */ SHORT GetNumVirtualKeys() override;
+    /* 00A4 */ CKeyInfo* GetVirtualKeys() override;
+    /* 00A8 */ BYTE* GetVirtualKeysFlags() override;
+
+    /* 01B0 */ BOOL m_bShiftKeyDown;
+    /* 01B4 */ BOOL m_bCapsLockKeyOn;
+    /* 01B8 */ BOOLEAN field_1B8;
+    /* 0300 */ CKeyInfo m_pVirtualKeys[CSCREENCHARACTER_VIRTUAL_KEYS];
+    /* 05D0 */ BYTE m_pVirtualKeysFlags[CSCREENCHARACTER_VIRTUAL_KEYS];
+    /* 07B4 */ BOOLEAN m_bCtrlKeyDown;
 };
 
 #endif /* CSCREENCHARACTER_H_ */
