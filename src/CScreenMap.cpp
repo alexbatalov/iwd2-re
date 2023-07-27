dbb553f645f1cca933c8887d1d1a071d262252bf
Alexander Batalov <alex.batalov@gmail.com>
2023-07-27 09:23:16 +0300

    Add CUIControlButtonMapNoteDone

--- src/CScreenMap.cpp
+++ src/CScreenMap.cpp
@@ -21,6 +21,12 @@ void CScreenMap::OnErrorButtonClick(INT nButton)
     // TODO: Incomplete.
 }
 
+// 0x6424D0
+void CScreenMap::OnDoneButtonClick()
+{
+    // TODO: Incomplete.
+}
+
 // -----------------------------------------------------------------------------
 
 // 0x6458B0
@@ -44,4 +50,29 @@ void CUIControlButtonMapError::OnLButtonClick(CPoint pt)
     UTIL_ASSERT(pMap != NULL);
 
     pMap->OnErrorButtonClick(m_nID);
+}
+
+// -----------------------------------------------------------------------------
+
+// 0x6459E0
+CUIControlButtonMapNoteDone::CUIControlButtonMapNoteDone(CUIPanel* panel, UI_CONTROL_BUTTON* controlInfo)
+    : CUIControlButton(panel, controlInfo, LBUTTON, 0)
+{
+}
+
+// 0x645A30
+CUIControlButtonMapNoteDone::~CUIControlButtonMapNoteDone()
+{
+}
+
+// 0x645AD0
+void CUIControlButtonMapNoteDone::OnLButtonClick(CPoint pt)
+{
+    CScreenMap* pMap = g_pBaldurChitin->m_pEngineMap;
+
+    // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenMap.cpp
+    // __LINE__: 4246
+    UTIL_ASSERT(pMap != NULL);
+
+    pMap->OnDoneButtonClick();
 }
--- src/CScreenMap.h
+++ src/CScreenMap.h
@@ -10,12 +10,20 @@ public:
     ~CScreenMap();
 
     void OnErrorButtonClick(INT nButton);
+    void OnDoneButtonClick();
 };
 
 class CUIControlButtonMapError : public CUIControlButton {
 public:
     CUIControlButtonMapError(CUIPanel* panel, UI_CONTROL_BUTTON* controlInfo);
     ~CUIControlButtonMapError() override;
+    void OnLButtonClick(CPoint pt) override;
+};
+
+class CUIControlButtonMapNoteDone : public CUIControlButton {
+public:
+    CUIControlButtonMapNoteDone(CUIPanel* panel, UI_CONTROL_BUTTON* controlInfo);
+    ~CUIControlButtonMapNoteDone() override;
     void OnLButtonClick(CPoint pt) override;
 };
 
