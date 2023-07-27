6a64a1828608543b89f25ff20e64fd4ec9cd09af
Alexander Batalov <alex.batalov@gmail.com>
2023-07-27 09:17:30 +0300

    Add CUIControlButtonMapError

--- src/CScreenMap.cpp
+++ src/CScreenMap.cpp
@@ -1,5 +1,8 @@
 #include "CScreenMap.h"
 
+#include "CBaldurChitin.h"
+#include "CUtil.h"
+
 // 0x63F960
 CScreenMap::CScreenMap()
 {
@@ -10,4 +13,35 @@ CScreenMap::CScreenMap()
 CScreenMap::~CScreenMap()
 {
     // TODO: Incomplete.
+}
+
+// 0x6422D0
+void CScreenMap::OnErrorButtonClick(INT nButton)
+{
+    // TODO: Incomplete.
+}
+
+// -----------------------------------------------------------------------------
+
+// 0x6458B0
+CUIControlButtonMapError::CUIControlButtonMapError(CUIPanel* panel, UI_CONTROL_BUTTON* controlInfo)
+    : CUIControlButton(panel, controlInfo, LBUTTON, 0)
+{
+}
+
+// 0x645900
+CUIControlButtonMapError::~CUIControlButtonMapError()
+{
+}
+
+// 0x6459A0
+void CUIControlButtonMapError::OnLButtonClick(CPoint pt)
+{
+    CScreenMap* pMap = g_pBaldurChitin->m_pEngineMap;
+
+    // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenMap.cpp
+    // __LINE__: 4202
+    UTIL_ASSERT(pMap != NULL);
+
+    pMap->OnErrorButtonClick(m_nID);
 }
--- src/CScreenMap.h
+++ src/CScreenMap.h
@@ -2,11 +2,21 @@
 #define CSCREENMAP_H_
 
 #include "CBaldurEngine.h"
+#include "CUIControlButton.h"
 
 class CScreenMap : public CBaldurEngine {
 public:
     CScreenMap();
     ~CScreenMap();
+
+    void OnErrorButtonClick(INT nButton);
+};
+
+class CUIControlButtonMapError : public CUIControlButton {
+public:
+    CUIControlButtonMapError(CUIPanel* panel, UI_CONTROL_BUTTON* controlInfo);
+    ~CUIControlButtonMapError() override;
+    void OnLButtonClick(CPoint pt) override;
 };
 
 #endif /* CSCREENMAP_H_ */
