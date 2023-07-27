9c68a0da514a73a7ab97b8d4f0341830ae1b065c
Alexander Batalov <alex.batalov@gmail.com>
2023-07-27 13:19:42 +0300

    Add some CScreenMap functions

--- src/CScreenMap.cpp
+++ src/CScreenMap.cpp
@@ -1,7 +1,13 @@
 #include "CScreenMap.h"
 
 #include "CBaldurChitin.h"
+#include "CGameArea.h"
+#include "CInfCursor.h"
+#include "CInfGame.h"
+#include "CScreenWorld.h"
 #include "CScreenWorldMap.h"
+#include "CUIControlTextDisplay.h"
+#include "CUIPanel.h"
 #include "CUtil.h"
 
 // 0x63F960
@@ -10,10 +16,520 @@ CScreenMap::CScreenMap()
     // TODO: Incomplete.
 }
 
+// 0x49FC40
+BOOL CScreenMap::CheckSystemKeyCtrl()
+{
+    return TRUE;
+}
+
+// 0x640590
+void CScreenMap::SetSystemKeyCtrl(BOOLEAN bValue)
+{
+    m_bCtrlKeyDown = bValue;
+}
+
+// 0x6405A0
+BOOL CScreenMap::GetCtrlKey()
+{
+    return m_bCtrlKeyDown;
+}
+
+// 0x49FC40
+BOOL CScreenMap::CheckSystemKeyShift()
+{
+    return TRUE;
+}
+
+// 0x6405B0
+void CScreenMap::SetSystemKeyShift(BOOL bValue)
+{
+    m_bShiftKeyDown = bValue;
+}
+
+// 0x6405C0
+BOOL CScreenMap::GetShiftKey()
+{
+    return m_bShiftKeyDown;
+}
+
+// 0x49FC40
+BOOL CScreenMap::CheckSystemKeyCapsLock()
+{
+    return TRUE;
+}
+
+// 0x6405D0
+void CScreenMap::SetSystemKeyCapsLock(BOOL bValue)
+{
+    m_bCapsLockKeyOn = bValue;
+}
+
+// 0x6405E0
+BOOL CScreenMap::GetCapsLockKey()
+{
+    return m_bCapsLockKeyOn;
+}
+
+// 0x49FC40
+BOOL CScreenMap::CheckMouseMove()
+{
+    return TRUE;
+}
+
+// 0x66F4B0
+void CScreenMap::OnMouseMove(CPoint pt)
+{
+    m_cUIManager.OnMouseMove(pt);
+}
+
+// 0x686660
+SHORT CScreenMap::GetNumVirtualKeys()
+{
+    return CSCREENMAP_VIRTUAL_KEYS;
+}
+
+// 0x699610
+CKeyInfo* CScreenMap::GetVirtualKeys()
+{
+    return m_pVirtualKeys;
+}
+
+// 0x668C30
+BYTE* CScreenMap::GetVirtualKeysFlags()
+{
+    return m_pVirtualKeysFlags;
+}
+
 // 0x640610
 CScreenMap::~CScreenMap()
 {
+}
+
+// 0x6406B0
+void CScreenMap::EngineActivated()
+{
+    if (m_cUIManager.m_bInitialized) {
+        if (CChitin::byte_8FB950
+            && g_pChitin->cNetwork.GetSessionOpen() == TRUE
+            && g_pChitin->cNetwork.GetSessionHosting() == TRUE
+            && g_pChitin->cNetwork.GetServiceProvider() != CNetwork::SERV_PROV_NULL) {
+            g_pBaldurChitin->m_pEngineWorld->TogglePauseGame(0, 1, 0);
+        }
+
+        m_preLoadFontRealms.SetResRef(CResRef("REALMS"), FALSE, TRUE);
+        m_preLoadFontRealms.RegisterFont();
+
+        m_preLoadFontTool.SetResRef(CResRef("TOOLFONT"), FALSE, TRUE);
+        m_preLoadFontTool.RegisterFont();
+
+        m_preLoadFontStnSml.SetResRef(CResRef("STONESML"), FALSE, TRUE);
+        m_preLoadFontStnSml.RegisterFont();
+
+        if (!g_pBaldurChitin->m_pEngineWorld->m_bPaused) {
+            g_pBaldurChitin->GetObjectGame()->GetWorldTimer()->StartTime();
+        }
+
+        m_cUIManager.GetPanel(2)->m_bNeedMouseMove = TRUE;
+        UpdateMainPanel();
+
+        UpdateCursorShape(0);
+        CheckEnablePortaits(1);
+        CheckEnableLeftPanel();
+
+        g_pBaldurChitin->GetObjectCursor()->SetCursor(0, FALSE);
+        m_cUIManager.InvalidateRect(NULL);
+    }
+}
+
+// 0x640810
+void CScreenMap::EngineDeactivated()
+{
+    if (CChitin::byte_8FB950
+        && g_pChitin->cNetwork.GetSessionOpen() == TRUE
+        && g_pChitin->cNetwork.GetSessionHosting() == TRUE
+        && g_pChitin->cNetwork.GetServiceProvider() != CNetwork::SERV_PROV_NULL) {
+        g_pBaldurChitin->m_pEngineWorld->TogglePauseGame(0, 1, 0);
+    }
+
+    CUIPanel* pPanel = m_cUIManager.GetPanel(2);
+
+    m_preLoadFontRealms.Unload();
+    m_preLoadFontTool.Unload();
+    m_preLoadFontStnSml.Unload();
+
+    if (m_cUIManager.m_pFocusedControl != NULL) {
+        // NOTE: Uninline.
+        m_cUIManager.KillCapture();
+    }
+
+    if (!g_pBaldurChitin->m_pEngineWorld->m_bPaused
+        && g_pChitin->cNetwork.GetServiceProvider() == CNetwork::SERV_PROV_NULL) {
+        g_pBaldurChitin->GetObjectGame()->GetWorldTimer()->StopTime();
+    }
+
+    if (pPanel != NULL) {
+        CUIControlButtonMapAreaMap* pButton = static_cast<CUIControlButtonMapAreaMap*>(pPanel->GetControl(2));
+        if (pButton != NULL) {
+            pButton->SetMap(NULL);
+        }
+    }
+}
+
+// 0x640910
+void CScreenMap::EngineGameInit()
+{
+    m_cUIManager.fInit(this, CResRef("GUIMAP"), g_pBaldurChitin->field_4A28);
+
+    CPoint pt;
+    if (g_pBaldurChitin->field_4A28) {
+        pt.x = CVideo::SCREENWIDTH / 2 - CBaldurChitin::DEFAULT_SCREEN_WIDTH;
+        pt.y = CVideo::SCREENHEIGHT / 2 - CBaldurChitin::DEFAULT_SCREEN_HEIGHT;
+    } else {
+        pt.x = (CVideo::SCREENWIDTH - CBaldurChitin::DEFAULT_SCREEN_WIDTH) / 2;
+        pt.y = (CVideo::SCREENHEIGHT - CBaldurChitin::DEFAULT_SCREEN_HEIGHT) / 2;
+    }
+
+    m_cUIManager.ShiftPanels(pt);
+
+    for (int index = 0; index < 4; index++) {
+        m_cUIManager.AddPanel(&(g_pBaldurChitin->field_49B4[index]));
+    }
+
+    m_nSelectedCharacter = 0;
+    m_pCurrentScrollBar = NULL;
+
+    m_cUIManager.GetPanel(3)->SetActive(FALSE);
+    m_cUIManager.GetPanel(4)->SetActive(FALSE);
+    m_cUIManager.GetPanel(50)->SetActive(FALSE);
+    m_cUIManager.GetPanel(5)->SetActive(FALSE);
+
+    m_bSelectWorldOnUp = FALSE;
+    m_nLastPicked = -1;
+    m_noteStrRef = -1;
+}
+
+// 0x640A70
+void CScreenMap::OnKeyDown(SHORT nKeysFlags)
+{
+    CInfGame* pGame = g_pBaldurChitin->GetObjectGame();
+
+    if (nKeysFlags > 0) {
+        for (SHORT nKeyFlag = 0; nKeyFlag < nKeysFlags; nKeyFlag++) {
+            switch (m_pVirtualKeysFlags[nKeyFlag]) {
+            case VK_TAB:
+                m_cUIManager.ForceToolTip();
+                break;
+            case VK_RETURN:
+                if (GetTopPopup() != NULL) {
+                    OnDoneButtonClick();
+                }
+                break;
+            case VK_ESCAPE:
+                if (GetTopPopup() != NULL) {
+                    OnCancelButtonClick();
+                } else {
+                    SelectEngine(g_pBaldurChitin->m_pEngineWorld);
+                }
+                break;
+            case VK_SNAPSHOT:
+                g_pBaldurChitin->GetCurrentVideoMode()->PrintScreen();
+                break;
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+                if (GetTopPopup() == NULL) {
+                    OnPortraitLClick(m_pVirtualKeysFlags[nKeyFlag] - '1');
+                }
+                break;
+            default:
+                if (GetTopPopup() == NULL) {
+                    for (SHORT index = 0; index < CINFGAME_KEYMAP_SIZE; index) {
+                        // __FILE__: .\Include\InfGame.h
+                        // __LINE__: 1486
+                        UTIL_ASSERT(index >= 0 && index < CINFGAME_KEYMAP_SIZE);
+
+                        if (pGame->m_pKeymap[index] == m_pVirtualKeysFlags[nKeyFlag]
+                            && pGame->m_pKeymapFlags[index] == m_bCtrlKeyDown) {
+                            switch (index) {
+                            case 0:
+                                static_cast<CBaldurEngine*>(g_pBaldurChitin->pActiveEngine)->OnLeftPanelButtonClick(5);
+                                break;
+                            case 1:
+                                static_cast<CBaldurEngine*>(g_pBaldurChitin->pActiveEngine)->OnLeftPanelButtonClick(8);
+                                break;
+                            case 2:
+                                static_cast<CBaldurEngine*>(g_pBaldurChitin->pActiveEngine)->OnLeftPanelButtonClick(0);
+                                break;
+                            case 3:
+                                static_cast<CBaldurEngine*>(g_pBaldurChitin->pActiveEngine)->OnLeftPanelButtonClick(6);
+                                break;
+                            case 4:
+                                static_cast<CBaldurEngine*>(g_pBaldurChitin->pActiveEngine)->OnLeftPanelButtonClick(7);
+                                break;
+                            case 5:
+                                static_cast<CBaldurEngine*>(g_pBaldurChitin->pActiveEngine)->OnLeftPanelButtonClick(4);
+                                break;
+                            case 6:
+                                static_cast<CBaldurEngine*>(g_pBaldurChitin->pActiveEngine)->OnLeftPanelButtonClick(9);
+                                break;
+                            case 7:
+                                static_cast<CBaldurEngine*>(g_pBaldurChitin->pActiveEngine)->OnLeftPanelButtonClick(13);
+                                break;
+                            case 29:
+                                OnRestButtonClick();
+                                break;
+                            default:
+                                break;
+                            }
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
+
+// 0x49FC40
+BOOL CScreenMap::CheckMouseLButton()
+{
+    return TRUE;
+}
+
+// 0x640D80
+void CScreenMap::OnLButtonDblClk(CPoint pt)
+{
     // TODO: Incomplete.
+}
+
+// 0x636970
+void CScreenMap::OnLButtonDown(CPoint pt)
+{
+    g_pBaldurChitin->GetObjectCursor()->m_nState = 0;
+    m_cUIManager.OnLButtonDown(pt);
+}
+
+// 0x640E20
+void CScreenMap::OnLButtonUp(CPoint pt)
+{
+    g_pBaldurChitin->GetObjectCursor()->m_nState = 0;
+    m_cUIManager.OnLButtonUp(pt);
+
+    if (m_bSelectWorldOnUp) {
+        m_bSelectWorldOnUp = FALSE;
+        SelectEngine(g_pBaldurChitin->m_pEngineWorld);
+    }
+}
+
+// 0x49FC40
+BOOL CScreenMap::CheckMouseRButton()
+{
+    return TRUE;
+}
+
+// 0x6369D0
+void CScreenMap::OnRButtonDown(CPoint pt)
+{
+    g_pBaldurChitin->GetObjectCursor()->m_nState = 1;
+    m_cUIManager.OnRButtonDown(pt);
+}
+
+// 0x640E80
+void CScreenMap::OnRButtonUp(CPoint pt)
+{
+    g_pBaldurChitin->GetObjectCursor()->m_nState = 0;
+    m_cUIManager.OnRButtonUp(pt);
+    UpdateMainPanel();
+    m_cUIManager.InvalidateRect(NULL);
+}
+
+// 0x640ED0
+void CScreenMap::OnPortraitLClick(DWORD nPortrait)
+{
+    // TODO: Incomplete.
+}
+
+// 0x641130
+void CScreenMap::OnPortraitLDblClick(DWORD nPortrait)
+{
+    // TODO: Incomplete.
+}
+
+// 0x641270
+void CScreenMap::TimerAsynchronousUpdate()
+{
+    // TODO: Incomplete.
+}
+
+// 0x63BAA0
+void CScreenMap::TimerSynchronousUpdate()
+{
+    g_pBaldurChitin->GetObjectGame()->SynchronousUpdate();
+    m_cUIManager.Render();
+    pVidMode->Flip(TRUE);
+}
+
+// 0x6416A0
+void CScreenMap::EnableMainPanel(BOOL bEnable)
+{
+    CUIPanel* pMainPanel = m_cUIManager.GetPanel(2);
+    CUIPanel* pLeftPanel = m_cUIManager.GetPanel(0);
+    CUIPanel* pRightPanel = m_cUIManager.GetPanel(1);
+
+    // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenMap.cpp
+    // __LINE__: 1138
+    UTIL_ASSERT(pMainPanel != NULL);
+
+    // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenMap.cpp
+    // __LINE__: 1139
+    UTIL_ASSERT(pLeftPanel != NULL);
+
+    // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenMap.cpp
+    // __LINE__: 1140
+    UTIL_ASSERT(pRightPanel != NULL);
+
+    if (bEnable) {
+        PlayGUISound(RESREF_SOUND_WINDOWCLOSE);
+    }
+
+    pMainPanel->SetEnabled(bEnable);
+    pLeftPanel->SetEnabled(bEnable);
+    pRightPanel->SetEnabled(bEnable);
+
+    if (CVideo::SCREENWIDTH / (g_pBaldurChitin->field_4A28 ? 2 : 1) != CBaldurChitin::DEFAULT_SCREEN_WIDTH) {
+        m_cUIManager.GetPanel(-5)->SetEnabled(bEnable);
+        m_cUIManager.GetPanel(-4)->SetEnabled(bEnable);
+        m_cUIManager.GetPanel(-3)->SetEnabled(bEnable);
+        m_cUIManager.GetPanel(-2)->SetEnabled(bEnable);
+    }
+
+    CheckEnablePortaits(1);
+    CheckEnableLeftPanel();
+}
+
+// 0x641800
+void CScreenMap::UpdateMainPanel()
+{
+    // TODO: Incomplete.
+}
+
+// 0x641970
+CUIPanel* CScreenMap::GetTopPopup()
+{
+    return m_lPopupStack.GetTailPosition() != NULL ? m_lPopupStack.GetTail() : NULL;
+}
+
+// 0x641990
+void CScreenMap::SummonPopup(DWORD dwPopupId)
+{
+    // TODO: Incomplete.
+}
+
+// 0x641BA0
+void CScreenMap::DismissPopup()
+{
+    // TODO: Incomplete.
+}
+
+// 0x641D00
+void CScreenMap::ResetErrorPanel(CUIPanel* pPanel)
+{
+    switch (pPanel->m_nID) {
+    case 3:
+        m_nNumErrorButtons = 1;
+        break;
+    case 4:
+        m_nNumErrorButtons = 2;
+        break;
+    case 50:
+        m_nNumErrorButtons = 3;
+        break;
+    default:
+        // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenMap.cpp
+        // __LINE__: 1562
+        UTIL_ASSERT(FALSE);
+    }
+
+    STR_RES strRes;
+    g_pBaldurChitin->m_cTlkTable.Fetch(m_dwErrorTextId, strRes);
+
+    strRes.cSound.SetChannel(0, 0);
+    strRes.cSound.SetFireForget(TRUE);
+    strRes.cSound.Play(TRUE);
+
+    CUIControlTextDisplay* pText = static_cast<CUIControlTextDisplay*>(pPanel->GetControl(3));
+
+    // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenMap.cpp
+    // __LINE__: 1575
+    UTIL_ASSERT(pText != NULL);
+
+    pText->RemoveAll();
+    UpdateText(pText, "%s", strRes.szText);
+
+    for (INT nButton = 0; nButton < m_nNumErrorButtons; nButton++) {
+        CUIControlButton* pButton = static_cast<CUIControlButton*>(pPanel->GetControl(nButton + 1));
+
+        // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenMap.cpp
+        // __LINE__: 1584
+        UTIL_ASSERT(pButton != NULL);
+
+        // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenMap.cpp
+        // __LINE__: 1752
+        UTIL_ASSERT(0 <= nButton && nButton < CSCREENMAP_ERROR_BUTTONS);
+
+        pButton->SetText(FetchString(m_strErrorButtonText[nButton]));
+    }
+}
+
+// 0x641F10
+void CScreenMap::ResetAreaNotePanel(CUIPanel* pPanel)
+{
+    // TODO: Incomplete.
+}
+
+// 0x642130
+void CScreenMap::OnRestButtonClick()
+{
+    CInfGame* pGame = g_pBaldurChitin->GetObjectGame();
+
+    // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenMap.cpp
+    // __LINE__: 1674
+    UTIL_ASSERT(pGame != NULL);
+
+    CSingleLock renderLock(&(m_cUIManager.field_36), FALSE);
+    renderLock.Lock(INFINITE);
+
+    STRREF strError;
+    if (pGame->CanRestParty(strError, 0, 0, 0)) {
+        m_nErrorState = 1;
+        m_dwErrorTextId = 15358;
+        m_strErrorButtonText[0] = 17199;
+        m_strErrorButtonText[1] = 11596;
+        m_strErrorButtonText[2] = 13727;
+        SummonPopup(50);
+    } else {
+        m_nErrorState = 0;
+        m_dwErrorTextId = strError;
+        m_strErrorButtonText[0] = 11973;
+        SummonPopup(3);
+    }
+
+    renderLock.Unlock();
+}
+
+// 0x642240
+void CScreenMap::UpdateNoteText(STRREF strRef)
+{
+    if (m_noteStrRef != strRef) {
+        m_noteStrRef = strRef;
+
+        CString sString = FetchString(strRef);
+        UpdateLabel(GetManager()->GetPanel(2), 0x10000003, "%s", sString);
+    }
 }
 
 // 0x6422D0
@@ -25,13 +541,24 @@ void CScreenMap::OnErrorButtonClick(INT nButton)
 // 0x642460
 void CScreenMap::OnCancelButtonClick()
 {
-    // TODO: Incomplete.
+    CSingleLock renderLock(&(m_cUIManager.field_36), FALSE);
+    renderLock.Lock(INFINITE);
+    DismissPopup();
+    renderLock.Unlock();
 }
 
 // 0x6424D0
 void CScreenMap::OnDoneButtonClick()
 {
     // TODO: Incomplete.
+}
+
+// 0x6426C0
+void CScreenMap::CancelEngine()
+{
+    while (GetTopPopup() != NULL) {
+        OnCancelButtonClick();
+    }
 }
 
 // -----------------------------------------------------------------------------
@@ -70,6 +597,27 @@ void CUIControlButtonMapWorld::OnLButtonClick(CPoint pt)
     pMap->SelectEngine(pWorldMap);
 
     renderLock.Unlock();
+}
+
+// -----------------------------------------------------------------------------
+
+// 0x6428D0
+CUIControlButtonMapAreaMap::CUIControlButtonMapAreaMap(CUIPanel* panel, UI_CONTROL_BUTTON* controlInfo)
+    : CUIControlButton(panel, controlInfo, LBUTTON | RBUTTON, 0)
+{
+    // TODO: Incomplete.
+}
+
+// 0x642B80
+CUIControlButtonMapAreaMap::~CUIControlButtonMapAreaMap()
+{
+    // TODO: Incomplete.
+}
+
+// 0x644CC0
+void CUIControlButtonMapAreaMap::SetMap(CGameArea* pArea)
+{
+    // TODO: Incomplete.
 }
 
 // -----------------------------------------------------------------------------
--- src/CScreenMap.h
+++ src/CScreenMap.h
@@ -2,16 +2,80 @@
 #define CSCREENMAP_H_
 
 #include "CBaldurEngine.h"
+#include "CKeyInfo.h"
 #include "CUIControlButton.h"
+#include "CVidFont.h"
+
+#define CSCREENMAP_VIRTUAL_KEYS 90
+#define CSCREENMAP_ERROR_BUTTONS 3
+
+class CGameArea;
+class CUIPanel;
 
 class CScreenMap : public CBaldurEngine {
 public:
     CScreenMap();
-    ~CScreenMap();
+    /* 0004 */ ~CScreenMap() override;
+    /* 0014 */ void EngineActivated() override;
+    /* 0018 */ void EngineDeactivated() override;
+    /* 0024 */ void EngineGameInit() override;
+    /* 0030 */ BOOL CheckSystemKeyCtrl() override;
+    /* 0034 */ void SetSystemKeyCtrl(BOOLEAN bValue) override;
+    /* 0038 */ BOOL GetCtrlKey() override;
+    /* 003C */ BOOL CheckSystemKeyShift() override;
+    /* 0040 */ void SetSystemKeyShift(BOOL bValue) override;
+    /* 0044 */ BOOL GetShiftKey() override;
+    /* 0050 */ BOOL CheckSystemKeyCapsLock() override;
+    /* 0054 */ void SetSystemKeyCapsLock(BOOL bValue) override;
+    /* 0058 */ BOOL GetCapsLockKey() override;
+    /* 0060 */ BOOL CheckMouseMove() override;
+    /* 0064 */ void OnMouseMove(CPoint pt) override;
+    /* 0068 */ BOOL CheckMouseLButton() override;
+    /* 006C */ void OnLButtonDblClk(CPoint pt) override;
+    /* 0070 */ void OnLButtonDown(CPoint pt) override;
+    /* 0074 */ void OnLButtonUp(CPoint pt) override;
+    /* 0088 */ BOOL CheckMouseRButton() override;
+    /* 0090 */ void OnRButtonDown(CPoint pt) override;
+    /* 0094 */ void OnRButtonUp(CPoint pt) override;
+    /* 00A0 */ SHORT GetNumVirtualKeys() override;
+    /* 00A4 */ CKeyInfo* GetVirtualKeys() override;
+    /* 00A8 */ BYTE* GetVirtualKeysFlags() override;
+    /* 00AC */ void OnKeyDown(SHORT nKeysFlags) override;
+    /* 00E0 */ void OnPortraitLClick(DWORD nPortrait) override;
+    /* 00E4 */ void OnPortraitLDblClick(DWORD nPortrait) override;
+    /* 00C0 */ void TimerAsynchronousUpdate() override;
+    /* 00C4 */ void TimerSynchronousUpdate() override;
+    /* 00F0 */ void OnRestButtonClick() override;
+    /* 0104 */ void CancelEngine() override;
 
+    void EnableMainPanel(BOOL bEnable);
+    void UpdateMainPanel();
+    CUIPanel* GetTopPopup();
+    void SummonPopup(DWORD dwPopupId);
+    void DismissPopup();
+    void ResetErrorPanel(CUIPanel* pPanel);
+    void ResetAreaNotePanel(CUIPanel* pPanel);
+    void UpdateNoteText(STRREF strRef);
     void OnErrorButtonClick(INT nButton);
     void OnCancelButtonClick();
     void OnDoneButtonClick();
+
+    /* 0106 */ CKeyInfo m_pVirtualKeys[CSCREENMAP_VIRTUAL_KEYS];
+    /* 03D6 */ BYTE m_pVirtualKeysFlags[CSCREENMAP_VIRTUAL_KEYS];
+    /* 0430 */ CTypedPtrList<CPtrList, CUIPanel*> m_lPopupStack;
+    /* 044C */ SHORT m_nLastPicked;
+    /* 044E */ INT m_nErrorState;
+    /* 0452 */ STRREF m_dwErrorTextId;
+    /* 0456 */ INT m_nNumErrorButtons;
+    /* 045A */ STRREF m_strErrorButtonText[CSCREENMAP_ERROR_BUTTONS];
+    /* 0466 */ BOOLEAN m_bSelectWorldOnUp;
+    /* 0468 */ CVidFont m_preLoadFontRealms;
+    /* 0968 */ CVidFont m_preLoadFontTool;
+    /* 0E68 */ CVidFont m_preLoadFontStnSml;
+    /* 1368 */ STRREF m_noteStrRef;
+    /* 136C */ BOOL m_bShiftKeyDown;
+    /* 1370 */ BOOL m_bCapsLockKeyOn;
+    /* 1374 */ BOOL m_bCtrlKeyDown;
 };
 
 class CUIControlButtonMapWorld : public CUIControlButton {
@@ -19,6 +83,14 @@ public:
     CUIControlButtonMapWorld(CUIPanel* panel, UI_CONTROL_BUTTON* controlInfo);
     ~CUIControlButtonMapWorld() override;
     void OnLButtonClick(CPoint pt) override;
+};
+
+class CUIControlButtonMapAreaMap : public CUIControlButton {
+public:
+    CUIControlButtonMapAreaMap(CUIPanel* panel, UI_CONTROL_BUTTON* controlInfo);
+    ~CUIControlButtonMapAreaMap() override;
+
+    void SetMap(CGameArea* pArea);
 };
 
 class CUIControlButtonMapError : public CUIControlButton {
