c879e8c6441620a389f7871a5a9b2e298b18b01a
Alexander Batalov <alex.batalov@gmail.com>
2023-08-07 11:42:39 +0300

    Add some CScreenWorldMap functions

--- src/CScreenWorldMap.cpp
+++ src/CScreenWorldMap.cpp
@@ -1,9 +1,216 @@
 #include "CScreenWorldMap.h"
 
+#include "CBaldurChitin.h"
+#include "CInfCursor.h"
+#include "CInfGame.h"
+#include "CScreenWorld.h"
+#include "CTimerWorld.h"
+#include "CUIControlEdit.h"
+#include "CUIControlScrollBar.h"
+#include "CUIPanel.h"
+#include "CUtil.h"
+
+// 0x8F89DC
+const LONG FATIGUE_TIME_CAP = 24 * CTimerWorld::TIMESCALE_MSEC_PER_HOUR;
+
 // 0x698860
 CScreenWorldMap::CScreenWorldMap()
 {
-    // TODO: Incomplete.
+    m_nEngineState = 0;
+    m_pMapControl = NULL;
+    field_FF2 = 0;
+    field_FF6 = 0;
+    field_FFE = 0;
+    m_nCurrentLink = 0;
+    m_bInControl = FALSE;
+    m_bClickedArea = FALSE;
+    field_1050 = 0;
+
+    SetVideoMode(0);
+
+    m_pVirtualKeys[0] = CKeyInfo(VK_ESCAPE, -1, 0);
+    m_pVirtualKeys[1] = CKeyInfo('1', 0, 0);
+    m_pVirtualKeys[2] = CKeyInfo('2', 0, 0);
+    m_pVirtualKeys[3] = CKeyInfo('3', 0, 0);
+    m_pVirtualKeys[4] = CKeyInfo('4', 0, 0);
+    m_pVirtualKeys[5] = CKeyInfo('5', 0, 0);
+    m_pVirtualKeys[6] = CKeyInfo('6', 0, 0);
+    m_pVirtualKeys[7] = CKeyInfo('7', 0, 0);
+    m_pVirtualKeys[8] = CKeyInfo('8', 0, 0);
+    m_pVirtualKeys[9] = CKeyInfo('9', 0, 0);
+    m_pVirtualKeys[10] = CKeyInfo(VK_SPACE, -1, 0);
+    m_pVirtualKeys[11] = CKeyInfo('A', 0, 0);
+    m_pVirtualKeys[12] = CKeyInfo('B', 0, 0);
+    m_pVirtualKeys[13] = CKeyInfo('C', 0, 0);
+    m_pVirtualKeys[14] = CKeyInfo('D', 0, 0);
+    m_pVirtualKeys[15] = CKeyInfo('E', 0, 0);
+    m_pVirtualKeys[16] = CKeyInfo('F', 0, 0);
+    m_pVirtualKeys[17] = CKeyInfo('G', 0, 0);
+    m_pVirtualKeys[18] = CKeyInfo('H', 0, 0);
+    m_pVirtualKeys[19] = CKeyInfo('I', 0, 0);
+    m_pVirtualKeys[20] = CKeyInfo('J', 0, 0);
+    m_pVirtualKeys[21] = CKeyInfo('K', 0, 0);
+    m_pVirtualKeys[22] = CKeyInfo('L', 0, 0);
+    m_pVirtualKeys[23] = CKeyInfo('M', 0, 0);
+    m_pVirtualKeys[24] = CKeyInfo('N', 0, 0);
+    m_pVirtualKeys[25] = CKeyInfo('O', 0, 0);
+    m_pVirtualKeys[26] = CKeyInfo('P', 0, 0);
+    m_pVirtualKeys[27] = CKeyInfo('Q', 0, 0);
+    m_pVirtualKeys[28] = CKeyInfo('R', 0, 0);
+    m_pVirtualKeys[29] = CKeyInfo('S', 0, 0);
+    m_pVirtualKeys[30] = CKeyInfo('T', 0, 0);
+    m_pVirtualKeys[31] = CKeyInfo('U', 0, 0);
+    m_pVirtualKeys[32] = CKeyInfo('V', 0, 0);
+    m_pVirtualKeys[33] = CKeyInfo('W', 0, 0);
+    m_pVirtualKeys[34] = CKeyInfo('X', 0, 0);
+    m_pVirtualKeys[35] = CKeyInfo('Y', 0, 0);
+    m_pVirtualKeys[36] = CKeyInfo('Z', 0, 0);
+    m_pVirtualKeys[37] = CKeyInfo('0', 0, 0);
+    m_pVirtualKeys[38] = CKeyInfo(VK_BACK, 0, 0);
+    m_pVirtualKeys[39] = CKeyInfo(VK_END, -1, 0);
+    m_pVirtualKeys[40] = CKeyInfo(VK_HOME, -1, 0);
+    m_pVirtualKeys[41] = CKeyInfo(VK_LEFT, 0, 0);
+    m_pVirtualKeys[42] = CKeyInfo(VK_UP, 0, 0);
+    m_pVirtualKeys[43] = CKeyInfo(VK_RIGHT, 0, 0);
+    m_pVirtualKeys[44] = CKeyInfo(VK_DOWN, 0, 0);
+    m_pVirtualKeys[45] = CKeyInfo(VK_SNAPSHOT, -1, 0);
+    m_pVirtualKeys[46] = CKeyInfo(VK_INSERT, -1, 0);
+    m_pVirtualKeys[47] = CKeyInfo(VK_DELETE, 0, 0);
+    m_pVirtualKeys[48] = CKeyInfo(VK_NUMPAD0, -1, 0);
+    m_pVirtualKeys[49] = CKeyInfo(VK_NUMPAD1, -1, 0);
+    m_pVirtualKeys[50] = CKeyInfo(VK_NUMPAD2, -1, 0);
+    m_pVirtualKeys[51] = CKeyInfo(VK_NUMPAD3, -1, 0);
+    m_pVirtualKeys[52] = CKeyInfo(VK_NUMPAD4, -1, 0);
+    m_pVirtualKeys[53] = CKeyInfo(VK_NUMPAD5, -1, 0);
+    m_pVirtualKeys[54] = CKeyInfo(VK_NUMPAD6, -1, 0);
+    m_pVirtualKeys[55] = CKeyInfo(VK_NUMPAD7, -1, 0);
+    m_pVirtualKeys[56] = CKeyInfo(VK_NUMPAD8, -1, 0);
+    m_pVirtualKeys[57] = CKeyInfo(VK_NUMPAD9, -1, 0);
+    m_pVirtualKeys[58] = CKeyInfo(VK_MULTIPLY, -1, 0);
+    m_pVirtualKeys[59] = CKeyInfo(VK_ADD, -1, 0);
+    m_pVirtualKeys[60] = CKeyInfo(VK_SEPARATOR, -1, 0);
+    m_pVirtualKeys[61] = CKeyInfo(VK_SUBTRACT, -1, 0);
+    m_pVirtualKeys[62] = CKeyInfo(VK_DECIMAL, -1, 0);
+    m_pVirtualKeys[63] = CKeyInfo(VK_DIVIDE, -1, 0);
+    m_pVirtualKeys[64] = CKeyInfo(VK_F1, -1, 0);
+    m_pVirtualKeys[65] = CKeyInfo(VK_F2, -1, 0);
+    m_pVirtualKeys[66] = CKeyInfo(VK_F3, -1, 0);
+    m_pVirtualKeys[67] = CKeyInfo(VK_F4, -1, 0);
+    m_pVirtualKeys[68] = CKeyInfo(VK_F5, -1, 0);
+    m_pVirtualKeys[69] = CKeyInfo(VK_F6, -1, 0);
+    m_pVirtualKeys[70] = CKeyInfo(VK_F7, -1, 0);
+    m_pVirtualKeys[71] = CKeyInfo(VK_F8, -1, 0);
+    m_pVirtualKeys[72] = CKeyInfo(VK_F9, -1, 0);
+    m_pVirtualKeys[73] = CKeyInfo(VK_F10, -1, 0);
+    m_pVirtualKeys[74] = CKeyInfo(VK_F11, -1, 0);
+    m_pVirtualKeys[75] = CKeyInfo(VK_F12, -1, 0);
+    m_pVirtualKeys[76] = CKeyInfo(VK_NUMLOCK, -1, 0);
+    m_pVirtualKeys[77] = CKeyInfo(VK_RETURN, -1, 0);
+    m_pVirtualKeys[78] = CKeyInfo(VK_OEM_3, 0, 0);
+    m_pVirtualKeys[79] = CKeyInfo(VK_OEM_MINUS, 0, 0);
+    m_pVirtualKeys[80] = CKeyInfo(VK_OEM_PLUS, 0, 0);
+    m_pVirtualKeys[81] = CKeyInfo(VK_OEM_4, 0, 0);
+    m_pVirtualKeys[82] = CKeyInfo(VK_OEM_6, 0, 0);
+    m_pVirtualKeys[83] = CKeyInfo(VK_OEM_5, 0, 0);
+    m_pVirtualKeys[84] = CKeyInfo(VK_OEM_1, 0, 0);
+    m_pVirtualKeys[85] = CKeyInfo(VK_OEM_7, 0, 0);
+    m_pVirtualKeys[86] = CKeyInfo(VK_OEM_COMMA, 0, 0);
+    m_pVirtualKeys[87] = CKeyInfo(VK_OEM_PERIOD, 0, 0);
+    m_pVirtualKeys[88] = CKeyInfo(VK_OEM_2, 0, 0);
+    m_pVirtualKeys[89] = CKeyInfo(VK_TAB, -1, 0);
+
+    m_bCtrlKeyDown = FALSE;
+    m_bShiftKeyDown = FALSE;
+    m_ptMapView.x = 0;
+    m_ptMapView.y = 0;
+    field_FCA = RGB(224, 180, 15);
+    m_vfLabel.SetColor(field_FCA, 0, FALSE);
+    field_FCE = -1;
+    m_nSelectedArea = -1;
+    m_bOverSelectedArea = FALSE;
+    m_nToolTip = 0;
+    m_ptMapStartMousePos.x = 0;
+    m_ptMapStartMousePos.y = 0;
+    m_ptMapStartView.x = 0;
+    m_ptMapStartView.y = 0;
+    m_bMapLeftDown = FALSE;
+    m_bMapDragging = FALSE;
+    m_pPath = NULL;
+    m_pMainPanel = NULL;
+    m_pChatDisplay = NULL;
+    field_104A = 0;
+}
+
+// 0x49FC40
+BOOL CScreenWorldMap::CheckSystemKeyShift()
+{
+    return TRUE;
+}
+
+// 0x6995A0
+void CScreenWorldMap::SetSystemKeyShift(BOOL bValue)
+{
+    m_bShiftKeyDown = bValue;
+}
+
+// 0x6995B0
+BOOL CScreenWorldMap::GetShiftKey()
+{
+    return m_bShiftKeyDown;
+}
+
+// 0x49FC40
+BOOL CScreenWorldMap::CheckSystemKeyCapsLock()
+{
+    return TRUE;
+}
+
+// 0x6995C0
+void CScreenWorldMap::SetSystemKeyCapsLock(BOOL bValue)
+{
+    m_bCapsLockKeyOn = bValue;
+}
+
+// 0x6995D0
+BOOL CScreenWorldMap::GetCapsLockKey()
+{
+    return m_bCapsLockKeyOn;
+}
+
+// 0x49FC40
+BOOL CScreenWorldMap::CheckSystemKeyCtrl()
+{
+    return TRUE;
+}
+
+// 0x6995E0
+void CScreenWorldMap::SetSystemKeyCtrl(BOOLEAN bValue)
+{
+    m_bCtrlKeyDown = bValue;
+}
+
+// 0x6995F0
+BOOL CScreenWorldMap::GetCtrlKey()
+{
+    return m_bCtrlKeyDown;
+}
+
+// 0x699600
+SHORT CScreenWorldMap::GetNumVirtualKeys()
+{
+    return CSCREENWORLDMAP_VIRTUAL_KEYS;
+}
+
+// 0x699610
+CKeyInfo* CScreenWorldMap::GetVirtualKeys()
+{
+    return m_pVirtualKeys;
+}
+
+// 0x699620
+BYTE* CScreenWorldMap::GetVirtualKeysFlags()
+{
+    return m_pVirtualKeysFlags;
 }
 
 // 0x699650
@@ -12,14 +219,176 @@ CScreenWorldMap::~CScreenWorldMap()
     // TODO: Incomplete.
 }
 
+// 0x699E40
+void CScreenWorldMap::EngineDeactivated()
+{
+    if (CChitin::byte_8FB950
+        && g_pChitin->cNetwork.GetSessionOpen() == TRUE
+        && g_pChitin->cNetwork.GetSessionHosting() == TRUE
+        && g_pChitin->cNetwork.GetServiceProvider() != CNetwork::SERV_PROV_NULL) {
+        if (!g_pBaldurChitin->m_pEngineWorld->m_bPaused) {
+            g_pBaldurChitin->m_pEngineWorld->TogglePauseGame(0, 1, 0);
+        }
+    }
+
+    m_preLoadFontRealms.Unload();
+    m_preLoadFontTool.Unload();
+
+    field_FE = 0;
+    field_102 = 0;
+
+    // NOTE: Uninline.
+    m_cUIManager.KillCapture();
+
+    m_bOverSelectedArea = FALSE;
+    m_bMapLeftDown = FALSE;
+    m_bMapDragging = FALSE;
+    field_FCE = -1;
+    m_nSelectedArea = -1;
+
+    g_pBaldurChitin->GetObjectGame()->field_1BA1 = 4;
+}
+
+// 0x49FC40
+BOOL CScreenWorldMap::CheckMouseMove()
+{
+    return TRUE;
+}
+
+// 0x69A400
+void CScreenWorldMap::OnMouseMove(CPoint pt)
+{
+    m_cUIManager.OnMouseMove(pt);
+    m_nToolTip = 0;
+}
+
+// 0x49FC40
+BOOL CScreenWorldMap::CheckMouseLButton()
+{
+    return TRUE;
+}
+
+// 0x5D76C0
+void CScreenWorldMap::OnLButtonDblClk(CPoint pt)
+{
+    m_cUIManager.OnLButtonDblClk(pt);
+}
+
+// 0x636970
+void CScreenWorldMap::OnLButtonDown(CPoint pt)
+{
+    g_pBaldurChitin->GetObjectCursor()->m_nState = 1;
+    m_cUIManager.OnLButtonDown(pt);
+}
+
+// 0x6369A0
+void CScreenWorldMap::OnLButtonUp(CPoint pt)
+{
+    g_pBaldurChitin->GetObjectCursor()->m_nState = 0;
+    m_cUIManager.OnLButtonUp(pt);
+}
+
+// 0x49FC40
+BOOL CScreenWorldMap::CheckMouseRButton()
+{
+    return TRUE;
+}
+
+// 0x6369D0
+void CScreenWorldMap::OnRButtonDown(CPoint pt)
+{
+    g_pBaldurChitin->GetObjectCursor()->m_nState = 1;
+    m_cUIManager.OnRButtonDown(pt);
+}
+
+// 0x636A00
+void CScreenWorldMap::OnRButtonUp(CPoint pt)
+{
+    g_pBaldurChitin->GetObjectCursor()->m_nState = 0;
+    m_cUIManager.OnRButtonUp(pt);
+}
+
+// 0x69A430
+void CScreenWorldMap::TimerAsynchronousUpdate()
+{
+    if (m_pChatDisplay != NULL) {
+        field_104A = g_pBaldurChitin->GetBaldurMessage()->m_cChatBuffer.UpdateTextDisplay(m_pChatDisplay, field_104A);
+    }
+
+    if (m_nEngineState == 0) {
+        g_pBaldurChitin->m_pEngineWorld->AsynchronousUpdate(FALSE);
+    }
+
+    OnMapAsyncUpdate();
+
+    UpdateCursorShape(0);
+    m_cUIManager.TimerAsynchronousUpdate();
+    g_pBaldurChitin->GetObjectCursor()->CursorUpdate(pVidMode);
+}
+
+// 0x63BAA0
+void CScreenWorldMap::TimerSynchronousUpdate()
+{
+    g_pBaldurChitin->GetObjectGame()->SynchronousUpdate();
+    m_cUIManager.Render();
+    pVidMode->Flip(TRUE);
+}
+
+// 0x69A4B0
+void CScreenWorldMap::UpdateMainPanel()
+{
+    m_pCurrentScrollBar = static_cast<CUIControlScrollBar*>(m_pMainPanel->GetControl(6));
+}
+
+// 0x69A9D0
+void CScreenWorldMap::OnMapAsyncUpdate()
+{
+    // TODO: Incomplete.
+}
+
 // 0x69C2A0
 void CScreenWorldMap::StartWorldMap(INT nEngineState, LONG nLeavingEdge, BOOLEAN bInControl)
 {
-    // TODO: Incomplete.
 }
 
 // 0x6A01C0
 void CScreenWorldMap::ClearChatMessages()
 {
     field_104A = 0;
+}
+
+// 0x6A01D0
+void CScreenWorldMap::GetChatEditBoxStatus(CString& sChatText, BOOL& bInputCapture)
+{
+    if (m_pMainPanel->m_nID == 3) {
+        CUIControlEdit* pEdit = static_cast<CUIControlEdit*>(m_pMainPanel->GetControl(7));
+
+        // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenWorldMap.cpp
+        // __LINE__: 4442
+        UTIL_ASSERT(pEdit != NULL);
+
+        sChatText = pEdit->GetText();
+        bInputCapture = m_cUIManager.m_pFocusedControl == pEdit;
+    } else {
+        sChatText = "";
+        bInputCapture = FALSE;
+    }
+}
+
+// 0x6A02A0
+void CScreenWorldMap::SetChatEditBoxStatus(const CString& sChatText, BOOL bInputCapture)
+{
+    if (m_pMainPanel->m_nID == 3) {
+        CUIControlEdit* pEdit = static_cast<CUIControlEdit*>(m_pMainPanel->GetControl(7));
+
+        // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenWorldMap.cpp
+        // __LINE__: 4480
+        UTIL_ASSERT(pEdit != NULL);
+
+        pEdit->SetText(sChatText);
+
+        if (bInputCapture) {
+            m_cUIManager.SetCapture(pEdit, CUIManager::KEYBOARD);
+        }
+    }
 }
--- src/CScreenWorldMap.h
+++ src/CScreenWorldMap.h
@@ -2,16 +2,92 @@
 #define CSCREENWORLDMAP_H_
 
 #include "CBaldurEngine.h"
+#include "CKeyInfo.h"
+#include "CVidCell.h"
+#include "CVidFont.h"
+#include "CVidMosaic.h"
+
+#define CSCREENWORLDMAP_VIRTUAL_KEYS 90
+
+class CUIControlTextDisplay;
+class CUIPanel;
 
 class CScreenWorldMap : public CBaldurEngine {
 public:
-    CScreenWorldMap();
-    ~CScreenWorldMap();
+    static const LONG FATIGUE_TIME_CAP;
 
+    CScreenWorldMap();
+    /* 0004 */ ~CScreenWorldMap() override;
+    /* 0018 */ void EngineDeactivated() override;
+    /* 0030 */ BOOL CheckSystemKeyCtrl() override;
+    /* 0034 */ void SetSystemKeyCtrl(BOOLEAN bValue) override;
+    /* 0038 */ BOOL GetCtrlKey() override;
+    /* 003C */ BOOL CheckSystemKeyShift() override;
+    /* 0040 */ void SetSystemKeyShift(BOOL bValue) override;
+    /* 0044 */ BOOL GetShiftKey() override;
+    /* 0050 */ BOOL CheckSystemKeyCapsLock() override;
+    /* 0054 */ void SetSystemKeyCapsLock(BOOL bValue) override;
+    /* 0058 */ BOOL GetCapsLockKey() override;
+    /* 0060 */ BOOL CheckMouseMove() override;
+    /* 0064 */ void OnMouseMove(CPoint pt) override;
+    /* 0068 */ BOOL CheckMouseLButton() override;
+    /* 006C */ void OnLButtonDblClk(CPoint pt) override;
+    /* 0070 */ void OnLButtonDown(CPoint pt) override;
+    /* 0074 */ void OnLButtonUp(CPoint pt) override;
+    /* 0088 */ BOOL CheckMouseRButton() override;
+    /* 0090 */ void OnRButtonDown(CPoint pt) override;
+    /* 0094 */ void OnRButtonUp(CPoint pt) override;
+    /* 00A0 */ SHORT GetNumVirtualKeys() override;
+    /* 00A4 */ CKeyInfo* GetVirtualKeys() override;
+    /* 00A8 */ BYTE* GetVirtualKeysFlags() override;
+    /* 00C0 */ void TimerAsynchronousUpdate() override;
+    /* 00C4 */ void TimerSynchronousUpdate() override;
+    /* 0110 */ void GetChatEditBoxStatus(CString& sChatText, BOOL& bInputCapture) override;
+    /* 0114 */ void SetChatEditBoxStatus(const CString& sChatText, BOOL bInputCapture) override;
+
+    void UpdateMainPanel();
+    void OnMapAsyncUpdate();
     void StartWorldMap(INT nEngineState, LONG nLeavingEdge, BOOLEAN bInControl);
     void ClearChatMessages();
 
+    /* 0106 */ CKeyInfo m_pVirtualKeys[CSCREENWORLDMAP_VIRTUAL_KEYS];
+    /* 03D6 */ BYTE m_pVirtualKeysFlags[CSCREENWORLDMAP_VIRTUAL_KEYS];
+    /* 0430 */ BOOLEAN m_bCtrlKeyDown;
+    /* 0431 */ BOOLEAN m_bShiftKeyDown;
+    /* 0432 */ BOOL m_bCapsLockKeyOn;
+    /* 0436 */ CTypedPtrList<CPtrList, CUIPanel*> m_lPopupStack;
+    /* 0452 */ INT m_nEngineState;
+    /* 0456 */ CPoint m_ptMapView;
+    /* 045E */ void* m_pMapControl;
+    /* 0462 */ CVidMosaic m_vmMap;
+    /* 0516 */ CVidCell m_vcAreas;
+    /* 05F0 */ CVidCell m_vcMarker;
+    /* 06CA */ CVidFont m_vfLabel;
+    /* 0FCA */ COLORREF field_FCA;
+    /* 0FCE */ int field_FCE;
+    /* 0FD2 */ INT m_nSelectedArea;
+    /* 0FD6 */ BOOL m_bOverSelectedArea;
+    /* 0FDA */ CPoint m_ptMapStartMousePos;
+    /* 0FE2 */ CPoint m_ptMapStartView;
+    /* 0FEA */ BOOL m_bMapLeftDown;
+    /* 0FEE */ BOOL m_bMapDragging;
+    /* 0FF2 */ int field_FF2;
+    /* 0FF6 */ int field_FF6;
+    /* 0FFA */ void* m_pPath;
+    /* 0FFE */ int field_FFE;
+    /* 1002 */ int m_nCurrentLink;
+    /* 1006 */ CResRef m_cResCurrentArea;
+    /* 100E */ CArray<CRect, CRect&> field_100E;
+    /* 1022 */ CCriticalSection field_1022;
+    /* 1042 */ CUIPanel* m_pMainPanel;
+    /* 1046 */ CUIControlTextDisplay* m_pChatDisplay;
     /* 104A */ int field_104A;
+    /* 104E */ BOOLEAN m_bInControl;
+    /* 104F */ BOOLEAN m_bClickedArea;
+    /* 1050 */ int field_1050;
+    /* 1054 */ int m_nToolTip;
+    /* 1058 */ CVidFont m_preLoadFontRealms;
+    /* 1558 */ CVidFont m_preLoadFontTool;
 };
 
 #endif /* CSCREENWORLDMAP_H_ */
