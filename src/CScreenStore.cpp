78bc1c6f3447769e25d9a520af0024dbe3a8eb0b
Alexander Batalov <alex.batalov@gmail.com>
2023-08-14 17:47:42 +0300

    Add some CScreenStore functions

--- src/CScreenStore.cpp
+++ src/CScreenStore.cpp
@@ -1,18 +1,299 @@
 #include "CScreenStore.h"
 
 #include "CBaldurChitin.h"
+#include "CInfCursor.h"
+#include "CInfGame.h"
+#include "CScreenWorld.h"
+#include "CUIControlEdit.h"
+#include "CUIPanel.h"
+#include "CUtil.h"
+
+// 0x8F8194
+const CString CScreenStore::TOKEN_HP("HP");
+
+// 0x8F8158
+const CString CScreenStore::TOKEN_ITEMNAME("ITEMNAME");
+
+// 0x8F8188
+const CString CScreenStore::TOKEN_ITEMCOST("ITEMCOST");
+
+// 0x8F8168
+CRITICAL_SECTION CScreenStore::m_critSect;
+
+// 0x8F8198
+BOOLEAN CScreenStore::m_bInCancelEngine;
 
 // 0x6709E0
 CScreenStore::CScreenStore()
 {
     // TODO: Incomplete.
+
+    InitializeCriticalSection(&m_critSect);
+}
+
+// 0x49FC40
+BOOL CScreenStore::CheckSystemKeyShift()
+{
+    return TRUE;
+}
+
+// 0x671790
+void CScreenStore::SetSystemKeyShift(BOOL bValue)
+{
+    m_bShiftKeyDown = bValue;
+}
+
+// 0x6717A0
+BOOL CScreenStore::GetShiftKey()
+{
+    return m_bShiftKeyDown;
+}
+
+// 0x49FC40
+BOOL CScreenStore::CheckSystemKeyCapsLock()
+{
+    return TRUE;
+}
+
+// 0x6717B0
+void CScreenStore::SetSystemKeyCapsLock(BOOL bValue)
+{
+    m_bCapsLockKeyOn = bValue;
+}
+
+// 0x6717C0
+BOOL CScreenStore::GetCapsLockKey()
+{
+    return m_bCapsLockKeyOn;
+}
+
+// 0x49FC40
+BOOL CScreenStore::CheckSystemKeyCtrl()
+{
+    return TRUE;
+}
+
+// 0x6717D0
+void CScreenStore::SetSystemKeyCtrl(BOOLEAN bValue)
+{
+    m_bCtrlKeyDown = bValue;
+}
+
+// 0x6717E0
+BOOL CScreenStore::GetCtrlKey()
+{
+    return m_bCtrlKeyDown;
+}
+
+// 0x6717F0
+SHORT CScreenStore::GetNumVirtualKeys()
+{
+    return CSCREENSTORE_VIRTUAL_KEYS;
+}
+
+// 0x699610
+CKeyInfo* CScreenStore::GetVirtualKeys()
+{
+    return m_pVirtualKeys;
+}
+
+// 0x671800
+BYTE* CScreenStore::GetVirtualKeysFlags()
+{
+    return m_pVirtualKeysFlags;
 }
 
 // 0x671830
 CScreenStore::~CScreenStore()
 {
     // TODO: Incomplete.
+
+    DeleteCriticalSection(&m_critSect);
 }
+
+// 0x671A20
+void CScreenStore::EngineActivated()
+{
+    // TODO: Incomplete.
+}
+
+// 0x671CC0
+void CScreenStore::EngineDeactivated()
+{
+    if (CChitin::byte_8FB950) {
+        if (g_pChitin->cNetwork.GetSessionOpen() == TRUE
+            && g_pChitin->cNetwork.GetSessionHosting() == TRUE
+            && g_pChitin->cNetwork.GetServiceProvider() != CNetwork::SERV_PROV_NULL) {
+            if (!g_pBaldurChitin->m_pEngineWorld->m_bPaused) {
+                g_pBaldurChitin->m_pEngineWorld->TogglePauseGame(0, 1, 0);
+            }
+        }
+    }
+
+    m_preLoadFontRealms.Unload();
+    m_preLoadFontStnSml.Unload();
+    m_preLoadFontTool.Unload();
+
+    // NOTE: Uninline.
+    m_cUIManager.KillCapture();
+}
+
+// 0x671D60
+void CScreenStore::EngineGameInit()
+{
+    // TODO: Incomplete.
+}
+
+// 0x671FA0
+void CScreenStore::EngineGameUninit()
+{
+    // NOTE: Uninline.
+    m_cUIManager.fUninit();
+}
+
+// 0x672D50
+void CScreenStore::TimerSynchronousUpdate()
+{
+    EnterCriticalSection(&m_critSect);
+
+    g_pBaldurChitin->GetObjectGame()->SynchronousUpdate();
+    m_cUIManager.Render();
+
+    for (INT nIndex = 0; nIndex < m_lPopupStack.GetCount(); nIndex++) {
+        POSITION pos = m_lPopupStack.FindIndex(nIndex);
+        if (pos != NULL) {
+            CUIPanel* pPanel = m_lPopupStack.GetAt(pos);
+            pPanel->SetActive(TRUE);
+            pPanel->SetInactiveRender(TRUE);
+            pPanel->InvalidateRect(NULL);
+
+            // NOTE: Inlining?
+            if (pPanel->m_nID != 20 && pPanel->m_nID == 12) {
+                CUIPanel* pPanel = m_cUIManager.GetPanel(12);
+                CUIControlBase* pControl = pPanel->GetControl(9);
+                pControl->SetActive(field_5A8);
+                pControl->SetInactiveRender(field_5A8);
+            }
+        }
+    }
+
+    LeaveCriticalSection(&m_critSect);
+
+    pVidMode->Flip(TRUE);
+}
+
+// 0x672E20
+void CScreenStore::EnableMainPanel(BOOL bEnable)
+{
+    // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenStore.cpp
+    // __LINE__: 1284
+    UTIL_ASSERT(m_pButtonBar != NULL);
+
+    // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenStore.cpp
+    // __LINE__: 1285
+    UTIL_ASSERT(m_pMainPanel != NULL);
+
+    if (bEnable) {
+        PlayGUISound(RESREF_SOUND_WINDOWCLOSE);
+    }
+
+    if (m_pMainPanel->m_nID == 2) {
+        m_pMainPanel->GetControl(50)->SetInactiveRender(field_4EC != 0);
+    }
+
+    m_pButtonBar->SetEnabled(bEnable);
+
+    if (CVideo::SCREENWIDTH / (g_pBaldurChitin->field_4A28 ? 2 : 1) != CBaldurChitin::DEFAULT_SCREEN_WIDTH) {
+        m_cUIManager.GetPanel(-5)->SetEnabled(bEnable);
+        m_cUIManager.GetPanel(-4)->SetEnabled(bEnable);
+        m_cUIManager.GetPanel(-3)->SetEnabled(bEnable);
+        m_cUIManager.GetPanel(-2)->SetEnabled(bEnable);
+    }
+
+    CheckEnablePanels(bEnable);
+}
+
+// NOTE: Inlined.
+CUIPanel* CScreenStore::GetTopPopup()
+{
+    return m_lPopupStack.GetTailPosition() != NULL ? m_lPopupStack.GetTail() : NULL;
+}
+
+// 0x673FE0
+void CScreenStore::UpdateMainPanel()
+{
+    // TODO: Incomplete.
+}
+
+// 0x673B60
+void CScreenStore::CheckEnablePanels(BOOL bEnable)
+{
+    // TODO: Incomplete.
+}
+
+// 0x67CE40
+void CScreenStore::OnCancelButtonClick()
+{
+    // TODO: Incomplete.
+}
+
+// 0x67E840
+void CScreenStore::CancelEngine()
+{
+    m_bInCancelEngine = TRUE;
+
+    while (GetTopPopup() != NULL) {
+        OnCancelButtonClick();
+    }
+
+    m_bInCancelEngine = FALSE;
+}
+
+// 0x67E420
+void CScreenStore::GetChatEditBoxStatus(CString& sChatText, BOOL& bInputCapture)
+{
+    if (m_pButtonBar->m_nID == 3) {
+        CUIControlEdit* pEdit = static_cast<CUIControlEdit*>(m_pMainPanel->GetControl(8));
+
+        // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenStore.cpp
+        // __LINE__: 9095
+        UTIL_ASSERT(pEdit != NULL);
+
+        sChatText = pEdit->GetText();
+        bInputCapture = m_cUIManager.m_pFocusedControl == pEdit;
+    } else {
+        sChatText = "";
+        bInputCapture = FALSE;
+    }
+}
+
+// 0x67E4F0
+void CScreenStore::SetChatEditBoxStatus(const CString& sChatText, BOOL bInputCapture)
+{
+    if (m_pButtonBar->m_nID == 15) {
+        CUIControlEdit* pEdit = static_cast<CUIControlEdit*>(m_pMainPanel->GetControl(8));
+
+        // __FILE__: C:\Projects\Icewind2\src\Baldur\InfScreenStore.cpp
+        // __LINE__: 9133
+        UTIL_ASSERT(pEdit != NULL);
+
+        pEdit->SetText(sChatText);
+
+        if (bInputCapture) {
+            m_cUIManager.SetCapture(pEdit, CUIManager::KEYBOARD);
+        }
+    }
+}
+
+// 0x67E870
+void CScreenStore::UpdatePartyGoldStatus()
+{
+    if (GetTopPopup() == NULL) {
+        UpdateMainPanel();
+    }
+}
+
+// -----------------------------------------------------------------------------
 
 // 0x683720
 CUIControlPortraitStore::CUIControlPortraitStore(CUIPanel* panel, UI_CONTROL_BUTTON* controlInfo)
--- src/CScreenStore.h
+++ src/CScreenStore.h
@@ -2,12 +2,69 @@
 #define CSCREENSTORE_H_
 
 #include "CBaldurEngine.h"
+#include "CKeyInfo.h"
 #include "CUIControlButton.h"
+#include "CVidFont.h"
+
+#define CSCREENSTORE_VIRTUAL_KEYS 91
+
+#define CSCREENSTORE_NUM_BOTTOMBUTTON 4
 
 class CScreenStore : public CBaldurEngine {
 public:
+    static const CString TOKEN_HP;
+    static const CString TOKEN_ITEMNAME;
+    static const CString TOKEN_ITEMCOST;
+
+    // FIXME: Make instance member.
+    static CRITICAL_SECTION m_critSect;
+
+    // FIXME: Make instance member.
+    static BOOLEAN m_bInCancelEngine;
+
     CScreenStore();
-    ~CScreenStore();
+    /* 0004 */ ~CScreenStore() override;
+    /* 0014 */ void EngineActivated() override;
+    /* 0018 */ void EngineDeactivated() override;
+    /* 0024 */ void EngineGameInit() override;
+    /* 0028 */ void EngineGameUninit() override;
+    /* 0030 */ BOOL CheckSystemKeyCtrl() override;
+    /* 0034 */ void SetSystemKeyCtrl(BOOLEAN bValue) override;
+    /* 0038 */ BOOL GetCtrlKey() override;
+    /* 003C */ BOOL CheckSystemKeyShift() override;
+    /* 0040 */ void SetSystemKeyShift(BOOL bValue) override;
+    /* 0044 */ BOOL GetShiftKey() override;
+    /* 0050 */ BOOL CheckSystemKeyCapsLock() override;
+    /* 0054 */ void SetSystemKeyCapsLock(BOOL bValue) override;
+    /* 0058 */ BOOL GetCapsLockKey() override;
+    /* 00A0 */ SHORT GetNumVirtualKeys() override;
+    /* 00A4 */ CKeyInfo* GetVirtualKeys() override;
+    /* 00A8 */ BYTE* GetVirtualKeysFlags() override;
+    /* 00C4 */ void TimerSynchronousUpdate() override;
+    /* 0104 */ void CancelEngine() override;
+    /* 010C */ void UpdatePartyGoldStatus() override;
+    /* 0110 */ void GetChatEditBoxStatus(CString& sChatText, BOOL& bInputCapture) override;
+    /* 0114 */ void SetChatEditBoxStatus(const CString& sChatText, BOOL bInputCapture) override;
+
+    void EnableMainPanel(BOOL bEnable);
+    CUIPanel* GetTopPopup();
+    void UpdateMainPanel();
+    void CheckEnablePanels(BOOL bEnable);
+    void OnCancelButtonClick();
+
+    /* 0106 */ CKeyInfo m_pVirtualKeys[CSCREENSTORE_VIRTUAL_KEYS];
+    /* 039E */ BYTE m_pVirtualKeysFlags[CSCREENSTORE_VIRTUAL_KEYS];
+    /* 0439 */ BOOLEAN m_bCtrlKeyDown;
+    /* 043A */ BOOLEAN m_bShiftKeyDown;
+    /* 043C */ BOOL m_bCapsLockKeyOn;
+    /* 0440 */ CUIPanel* m_pMainPanel;
+    /* 04EC */ int field_4EC;
+    /* 0588 */ CTypedPtrList<CPtrList, CUIPanel*> m_lPopupStack;
+    /* 05A8 */ int field_5A8;
+    /* 05B4 */ CUIPanel* m_pButtonBar;
+    /* 05DA */ CVidFont m_preLoadFontRealms;
+    /* 0ADA */ CVidFont m_preLoadFontStnSml;
+    /* 0FDA */ CVidFont m_preLoadFontTool;
 };
 
 class CUIControlPortraitStore : public CUIControlButton {
