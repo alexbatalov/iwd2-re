983c71c792171a0bd90a6ee034d296ad3fbb92a7
Alexander Batalov <alex.batalov@gmail.com>
2023-08-20 16:24:58 +0300

    Add CIcon

---
+++ src/CIcon.cpp
@@ -0,0 +1,13 @@
+#include "CIcon.h"
+
+// 0x8D7E68
+const CPoint CIcon::LAST_DIGIT_OFFSET(25, 25);
+
+// 0x8D7E60
+const CPoint CIcon::FIRST_DIGIT_OFFSET(2, 2);
+
+// 0x8D7E38
+const CSize CIcon::ICON_SIZE_SM(32, 32);
+
+// 0x8D7E58
+const CSize CIcon::ICON_SIZE_LG(64, 64);
---
+++ src/CIcon.h
@@ -0,0 +1,14 @@
+#ifndef CICON_H_
+#define CICON_H_
+
+#include "mfc.h"
+
+class CIcon {
+public:
+    static const CPoint LAST_DIGIT_OFFSET;
+    static const CPoint FIRST_DIGIT_OFFSET;
+    static const CSize ICON_SIZE_SM;
+    static const CSize ICON_SIZE_LG;
+};
+
+#endif /* CICON_H_ */
