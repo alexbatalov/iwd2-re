6f51d1a542bbccaa82280ef21a4909c78b331181
Alexander Batalov <alex.batalov@gmail.com>
2023-11-07 11:11:06 +0300

    Add CVidMode::FillRect3d

--- src/CVidMode.h
+++ src/CVidMode.h
@@ -61,6 +61,7 @@ public:
     BOOL DrawRect3d(const CRect& r, const CRect& rClip, COLORREF rgbColor);
     BOOL DrawEllipse3d(const CPoint& ptCenter, const CSize& axis, const CRect& rClip, COLORREF rgbColor);
     BOOL DrawRecticle3d(const CVIDMODE_RECTICLE_DESCRIPTION& rd, const CRect& rClip, COLORREF rgbColor);
+    static void FillRect3d(const VERTEX_DESC* pVertices);
     BOOL CheckResults3d(int rc);
     static void Set3dClipRect(const CRect& rClip);
     void EnableScissoring();
--- src/CVideo3d.cpp
+++ src/CVideo3d.cpp
@@ -1332,6 +1332,35 @@ BOOL CVidMode::DrawRecticle3d(const CVIDMODE_RECTICLE_DESCRIPTION& rd, const CRe
     return FALSE;
 }
 
+// 0x7BD140
+void CVidMode::FillRect3d(const VERTEX_DESC* pVertices)
+{
+    CVideo3d::glDisable(GL_TEXTURE_2D);
+    g_pChitin->GetCurrentVideoMode()->CheckResults3d(0);
+
+    CVideo3d::glEnable(GL_BLEND);
+    g_pChitin->GetCurrentVideoMode()->CheckResults3d(0);
+
+    CVideo3d::glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    g_pChitin->GetCurrentVideoMode()->CheckResults3d(0);
+
+    CVideo3d::glBegin(GL_QUADS);
+
+    for (INT nIndex = 0; nIndex < 4; nIndex++) {
+        CVideo3d::glColor4f(pVertices[nIndex].color.fRed,
+            pVertices[nIndex].color.fGreen,
+            pVertices[nIndex].color.fBlue,
+            pVertices[nIndex].color.fAlpha);
+        CVideo3d::glVertex3f(pVertices[nIndex].fX, pVertices[nIndex].fY, 0.0f);
+    }
+
+    CVideo3d::glEnd();
+    g_pChitin->GetCurrentVideoMode()->CheckResults3d(0);
+
+    CVideo3d::glDisable(GL_BLEND);
+    g_pChitin->GetCurrentVideoMode()->CheckResults3d(0);
+}
+
 // 0x7BD270
 void CVidMode::RenderTint3d(COLORREF rgbTint, const CRect& rClip)
 {
