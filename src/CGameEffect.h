cc2ba341003d9c207da8bed51ff36c740899cb85
Alexander Batalov <alex.batalov@gmail.com>
2023-11-13 10:02:34 +0300

    Make CGameEffect::AddPortraitIcon non-static

--- src/CGameEffect.h
+++ src/CGameEffect.h
@@ -308,7 +308,7 @@ public:
     void SetSourceId(LONG sourceID);
 
     void AddSlowEffect(CGameSprite* pSprite);
-    static void AddPortraitIcon(CGameSprite* pSprite, int icon);
+    void AddPortraitIcon(CGameSprite* pSprite, int icon);
     static void ClearItemEffect(ITEM_EFFECT* itemEffect, WORD newEffectId);
 
     /* 010C */ LONG m_sourceID;
