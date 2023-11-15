6b9cafed2ca4bc5371af07364d88197ab47159cd
Alexander Batalov <alex.batalov@gmail.com>
2023-11-15 15:54:17 +0300

    Add CGameSprite::ReadyOffInternalList

--- src/CGameSprite.cpp
+++ src/CGameSprite.cpp
@@ -2038,6 +2038,49 @@ void CGameSprite::ReadyItem(SHORT buttonNum, BOOLEAN firstCall)
     }
 }
 
+// FIXME: `buttonData` should be reference.
+//
+// 0x719CA0
+void CGameSprite::ReadyOffInternalList(CButtonData buttonData, BOOLEAN firstCall)
+{
+    m_currentUseButton = buttonData;
+
+    CAIAction action;
+    CString string;
+    BOOL cursor = ReadyCursor();
+
+    if (!firstCall && !cursor) {
+        switch (m_currentUseButton.m_abilityId.m_targetType) {
+        case 5:
+            // FIXME: One time is usually enough.
+            g_pBaldurChitin->GetObjectGame()->SetLastTarget(CGameObjectArray::INVALID_INDEX);
+            g_pBaldurChitin->GetObjectGame()->SetLastTarget(CGameObjectArray::INVALID_INDEX);
+
+            // FIXME: Unused.
+            m_currentUseButton.m_abilityId.m_res.CopyToString(string);
+
+            // NOTE: Uninline.
+            action = CAIAction(CAIAction::USEITEM,
+                m_typeAI,
+                m_currentUseButton.m_abilityId.m_itemNum,
+                m_currentUseButton.m_abilityId.m_abilityNum,
+                0);
+
+            ClearActions(FALSE);
+            m_userCommandPause = 75;
+            m_triggerId = CGameObjectArray::INVALID_INDEX;
+            AddAction(action);
+            m_interrupt = TRUE;
+            break;
+        case 6:
+            // __FILE__: C:\Projects\Icewind2\src\Baldur\ObjCreature.cpp
+            // __LINE__: 18593
+            UTIL_ASSERT(FALSE);
+            break;
+        }
+    }
+}
+
 // 0x71B3B0
 void CGameSprite::CheckToolTipItem(BYTE buttonNum)
 {
--- src/CGameSprite.h
+++ src/CGameSprite.h
@@ -410,6 +410,7 @@ public:
     BOOL ReadyCursor();
     void ReadySpell(SHORT buttonNum, INT nType, BOOLEAN firstCall);
     void ReadyItem(SHORT buttonNum, BOOLEAN firstCall);
+    void ReadyOffInternalList(CButtonData buttonData, BOOLEAN firstCall);
     void CheckToolTipItem(BYTE buttonNum);
     void UnequipAll(BOOL a1);
     void EquipAll(BOOL a1);
